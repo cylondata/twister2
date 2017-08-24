5d6253dc74fa5a7586537de5bb7c5dd30dc65af1
pulasthi <pulasthi911@gmail.com>
2017-08-24 15:43:19 -0400

    fixing styles and adding interfaces

--- twister2/data/src/java/edu/iu/dsc/tws/data/fs/FileSystem.java
+++ twister2/data/src/java/edu/iu/dsc/tws/data/fs/FileSystem.java
@@ -1,3 +1,14 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 package edu.iu.dsc.tws.data.fs;
 
 /**
@@ -7,53 +18,53 @@ package edu.iu.dsc.tws.data.fs;
  */
 public abstract class FileSystem {
 
-    /**
-     * Check if the given path exsits
-     * @param path
-     * @return
-     */
-    public boolean exists(Path path){
-        return true;
-    }
+  /**
+   * Check if the given path exsits
+   * @param path
+   * @return
+   */
+  public boolean exists(Path path){
+    return true;
+  }
 
-    /**
-     * Check if file
-     * @param path
-     * @return
-     */
-    public boolean isFile(Path path){
-        return true;
-    }
+  /**
+   * Check if file
+   * @param path
+   * @return
+   */
+  public boolean isFile(Path path){
+    return true;
+  }
 
-    /**
-     * Check if directory
-     * @param path
-     * @return
-     */
-    public boolean isDirectory(Path path){
-        return true;
-    }
+  /**
+   * Check if directory
+   * @param path
+   * @return
+   */
+  public boolean isDirectory(Path path){
+    return true;
+  }
 
-    /**
-     * check if isSymlink
-     * @param path
-     * @return
-     */
-    public boolean isSymlink(Path path){
-        return true;
-    }
+  /**
+   * check if isSymlink
+   * @param path
+   * @return
+   */
+  public boolean isSymlink(Path path){
+    return true;
+  }
 
-    /**
-     * Set the working Directory
-     * @param path
-     */
-    public abstract void setWorkingDirectory(Path path);
+  /**
+   * Set the working Directory
+   * @param path
+   */
+  public abstract void setWorkingDirectory(Path path);
 
-    /**
-     * Get the working Directory
-     * @return
-     */
-    public abstract Path getWorkingDirectory();
+  /**
+   * Get the working Directory
+   * @return
+   */
+  public abstract Path getWorkingDirectory();
 
 
 }
