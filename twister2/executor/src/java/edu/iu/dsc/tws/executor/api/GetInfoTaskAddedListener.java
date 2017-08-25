da2f37e2e2a68c23aa36747feb566c5168368a72
Vibhatha Abeykoon <vibhatha@gmail.com>
2017-08-25 14:17:33 -0400

    new:dev: task executioner skeleton

---
+++ twister2/executor/src/java/edu/iu/dsc/tws/executor/api/GetInfoTaskAddedListener.java
@@ -0,0 +1,33 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.executor.api;
+
+import edu.iu.dsc.tws.executor.model.Task;
+
+/**
+ * Created by vibhatha on 8/25/17.
+ */
+public class GetInfoTaskAddedListener implements ITaskAddedListener {
+  @Override
+  public void onTaskAdded(Task task) {
+    System.out.println("Task Added ");
+    System.out.println(task.toString());
+  }
+
+  @Override
+  public void updateTaskAdded(Task task){
+    System.out.println("A new task added : "+task.getName());
+    System.out.println(task.toString());
+  }
+
+
+}
--- twister2/executor/src/java/edu/iu/dsc/tws/executor/model/Task.java
+++ twister2/executor/src/java/edu/iu/dsc/tws/executor/model/Task.java
@@ -12,7 +12,85 @@
 package edu.iu.dsc.tws.executor.model;
 
 
+import java.util.Date;
+
 public class Task {
 
+  private long id;
+  private String name;
+  private Date date;
+  private String description;
 
+  public Task(long id, String name, Date date, String description) {
+    this.id = id;
+    this.name = name;
+    this.date = date;
+    this.description = description;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Task)) return false;
+
+    Task task = (Task) o;
+
+    if (getId() != task.getId()) return false;
+    if (getName() != null ? !getName().equals(task.getName()) : task.getName() != null)
+      return false;
+    if (getDate() != null ? !getDate().equals(task.getDate()) : task.getDate() != null)
+      return false;
+    return getDescription() != null ? getDescription().equals(task.getDescription()) : task.getDescription() == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = (int) (getId() ^ (getId() >>> 32));
+    result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+    result = 31 * result + (getDate() != null ? getDate().hashCode() : 0);
+    result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Task{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", date=" + date +
+        ", description='" + description + '\'' +
+        '}';
+  }
 }
