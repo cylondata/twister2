611d4861af42ee79078a0222948851f5a3256231
kgovind <kannan.gridlab@gmail.com>
2017-09-11 16:16:17 -0400

    Task Scheduling Code Update...

--- twister2/taskscheduler/src/java/edu/iu/dsc/tws/tsched/utils/Job.java
+++ twister2/taskscheduler/src/java/edu/iu/dsc/tws/tsched/utils/Job.java
@@ -22,6 +22,18 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package edu.iu.dsc.tws.tsched.utils;
 
 //This class will be replaced with the original Job file from job package.
@@ -36,7 +48,7 @@ public class Job {
   public Job() {
   }
 
-  public static void setJobId(int jobId) {
+  public void setJobId(int jobId) {
     Job.jobId = jobId;
   }
 
