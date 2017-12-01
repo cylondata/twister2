c713fd4cef1d41517e288775ac24105a8a101da3
kgovind <kannan.gridlab@gmail.com>
2017-12-01 15:00:59 -0500

    Updating Abstract Task Graph Code...

--- twister2/task/src/main/java/edu/iu/dsc/tws/task/taskgraphbuilder/AbstractTaskGraph.java
+++ twister2/task/src/main/java/edu/iu/dsc/tws/task/taskgraphbuilder/AbstractTaskGraph.java
@@ -21,6 +21,18 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 package edu.iu.dsc.tws.task.taskgraphbuilder;
 
 import java.util.Collection;
@@ -28,23 +40,24 @@ import java.util.Set;
 
 public abstract class AbstractTaskGraph<TV, TE> implements TaskGraph<TV, TE> {
 
-  public AbstractTaskGraph() {
+  protected AbstractTaskGraph() {
   }
 
   @Override
   public boolean containsTaskEdge(TV sourceTaskVertex, TV targetTaskVertex) {
-    boolean success = false;
+    boolean flag = false;
     if (!(getTaskEdge(sourceTaskVertex, targetTaskVertex) == null)) {
-      success = true;
+      flag = true;
     }
-    return success;
+    return flag;
   }
 
   @Override
   public boolean removeAllTaskEdges(Collection<? extends TE> taskEdges) {
     boolean success = false;
     for (TE taskEdge : taskEdges) {
-      // success |= removeTaskEdge(taskEdge);
+      //success |= removeTaskEdge (taskEdge);
+      success = true;
     }
     return success;
   }
@@ -52,22 +65,33 @@ public abstract class AbstractTaskGraph<TV, TE> implements TaskGraph<TV, TE> {
   @Override
   public Set<TE> removeAllTaskEdges(TV sourceTaskVertex,
                                     TV targetTaskVertex) {
-    return null;
-  }
-
-  protected boolean removeAllTaskEdges(TE[] taskEdges) {
-    boolean success = false;
-    //do the computation to remove all the task edges....!
-    return success;
+    Set<TE> removedTaskEdge = getAllTaskEdges(sourceTaskVertex, targetTaskVertex);
+    if (removedTaskEdge == null) {
+      return null;
+    }
+    removeAllTaskEdges(removedTaskEdge);
+    return removedTaskEdge;
   }
 
   @Override
   public boolean removeAllTaskVertices(Collection<? extends TV>
                                            taskVertices) {
-    boolean success = false;
-    //do the computation to remove the vertex....
-    return success;
+    boolean flag = false;
+    for (TV taskVertex : taskVertices) {
+      flag |= removeTaskVertex(taskVertex);
+    }
+    return flag;
   }
 
-
+  protected boolean assertTaskVertexExist(TV taskVertex) {
+    if (containsTaskVertex(taskVertex)) {
+      return true;
+    } else if (taskVertex == null) {
+      throw new NullPointerException();
+    } else {
+      throw new IllegalArgumentException(
+          "no such vertex in graph: " + taskVertex.toString());
+    }
+  }
 }
+
