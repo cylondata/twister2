bd4630e97b59eaca5466d8a27bfcc818e14da435
kgovind <kannan.gridlab@gmail.com>
2017-12-12 16:28:03 -0500

    Fixed the code style error in the taskgraph builder...

--- twister2/task/src/main/java/edu/iu/dsc/tws/task/taskgraphbuilder/AbstractTaskGraphImpl.java
+++ twister2/task/src/main/java/edu/iu/dsc/tws/task/taskgraphbuilder/AbstractTaskGraphImpl.java
@@ -21,6 +21,18 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 package edu.iu.dsc.tws.task.taskgraphbuilder;
 
 import java.io.Serializable;
@@ -45,7 +57,8 @@ public abstract class AbstractTaskGraphImpl<TV, TE>
   private TaskGraphSpecifics taskGraphSpecifics;
   private Map<TE, IntrusiveTaskEdge> taskEdgeMap;
   private TaskEdgeSetFactory<TV, TE> taskEdgeSetFactory;
-  private transient TypeUtil<TV> vertexTypeDecl = null;
+  //private transient TypeUtil<TV> vertexTypeDecl = null;
+  private transient TypeUtil<TV> vertexTypeDecl = new TypeUtil<>();
   private transient Set<TE> unmodifiableTaskEdgeSet = null;
   private transient Set<TV> unmodifiableTaskVertexSet = null;
 
@@ -254,7 +267,11 @@ public abstract class AbstractTaskGraphImpl<TV, TE>
 
   @Override
   public TV getTaskEdgeSource(TE taskEdge) {
-    return TypeUtil.uncheckedCast(
+    /*return TypeUtil.uncheckedCast(
+        getIntrusiveTaskEdge(taskEdge).source,
+        vertexTypeDecl);*/
+
+    return vertexTypeDecl.uncheckedCast(
         getIntrusiveTaskEdge(taskEdge).source,
         vertexTypeDecl);
 
@@ -262,7 +279,11 @@ public abstract class AbstractTaskGraphImpl<TV, TE>
 
   @Override
   public TV getTaskEdgeTarget(TE taskEdge) {
-    return TypeUtil.uncheckedCast(
+
+    /*return TypeUtil.uncheckedCast(
+        getIntrusiveTaskEdge(taskEdge).target,
+        vertexTypeDecl);*/
+    return vertexTypeDecl.uncheckedCast(
         getIntrusiveTaskEdge(taskEdge).target,
         vertexTypeDecl);
   }
