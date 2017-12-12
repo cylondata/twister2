165ee38815ad8fe99b8f7bdf0908423aad809e62
kgovind <kannan.gridlab@gmail.com>
2017-12-12 16:28:21 -0500

    Fixed the code style error in the taskgraph builder...

--- twister2/task/src/main/java/edu/iu/dsc/tws/task/taskgraphbuilder/DataflowTaskGraph.java
+++ twister2/task/src/main/java/edu/iu/dsc/tws/task/taskgraphbuilder/DataflowTaskGraph.java
@@ -9,6 +9,18 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 package edu.iu.dsc.tws.task.taskgraphbuilder;
 
 import java.util.LinkedHashMap;
@@ -40,11 +52,6 @@ public class DataflowTaskGraph<TV, TE> extends AbstractTaskGraphImpl<TV, TE>
   @Override
   public Set<TV> getTaskVertexSet() {
     return vertexMapDataflow.keySet();
-  }
-
-  @Override
-  public Set<TV> taskVertexSet() {
-    return null;
   }
 
   @Override
