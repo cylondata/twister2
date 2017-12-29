67cfcaa6533d9fa22ebc970c075c80aa6d161ed0
supunkamburugamuve <supun06@gmail.com>
2017-12-29 00:06:07 -0500

    Fixing serialization of multi objects for aggregations

--- twister2/config/src/yaml/conf/slurmmpi/mpi.sh
+++ twister2/config/src/yaml/conf/slurmmpi/mpi.sh
@@ -10,6 +10,6 @@ if [ $em != $2 ]; then
 fi
 
 echo mpirun -np $1 --hostfile $7 java -Djava.util.loggi.config.file=conf/logger.properties $cp -cp $3 edu.iu.dsc.tws.rsched.schedulers.slurmmpi.MPIProcess --container_class $4 --twister2_home $5 --cluster_name nodesmpi --config_dir $6
-mpirun java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djava.util.loggi.config.file=conf/logger.properties $cp -cp $3 edu.iu.dsc.tws.rsched.schedulers.mpi.MPIProcess --container_class $4 --twister2_home $5 --cluster_name nodesmpi --config_dir $6 &
+mpirun java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djava.util.loggi.config.file=conf/logger.properties $cp -cp $3 edu.iu.dsc.tws.rsched.schedulers.mpi.MPIProcess --container_class $4 --twister2_home $5 --cluster_name slurmmpi --config_dir $6 &
 
 wait
