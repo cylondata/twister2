8d4173f37462b4de3818a437aea56ca8ede9b4cd
kgovind <kannan.gridlab@gmail.com>
2018-01-16 13:26:54 -0500

    Updating the multitask taskgraph example...!

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/MultiTaskGraphExample.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/MultiTaskGraphExample.java
@@ -21,6 +21,18 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 package edu.iu.dsc.tws.examples;
 
 import java.util.ArrayList;
@@ -109,6 +121,10 @@ public class MultiTaskGraphExample implements IContainer {
    *        |                        |
    *        V               (2)      V
    *   SinkTask2 (Task 2) -----> SinkTask3 (Task 3)
+   *
+   *   Here, (1) represents Task 1 and Task 2 starts simultaneously (receive input
+    *  from source task (Task 0), whereas (2) represents Task 3 receives input
+    *  from Task 1 & Task 2.
    */
 
     dataflowTaskGraph = new DataflowTaskGraphGenerator()
@@ -128,7 +144,7 @@ public class MultiTaskGraphExample implements IContainer {
         taskExecutor.registerTask(parsedTaskSet.iterator().next());
         taskExecutor.submitTask(0);
         taskExecutor.progres();
-      } else if (containerId == 1) { //This loop should be modified for the complex task graphs
+      } else if (containerId == 1) {
         int index = 0;
         for (Task processedTask : parsedTaskSet) {
           if (index == 0) {
