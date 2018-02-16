04255c9d44499f514eb6c6aa9c29174da2f0f8f2
pulasthi <pulasthi911@gmail.com>
2018-02-15 19:20:08 -0500

    adding data loader to terasort

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/basic/batch/terasort/TeraSortContainer.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/basic/batch/terasort/TeraSortContainer.java
@@ -11,7 +11,17 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.examples.basic.batch.terasort;
 
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.logging.Logger;
+
 import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.comms.core.TWSCommunication;
+import edu.iu.dsc.tws.comms.core.TWSNetwork;
+import edu.iu.dsc.tws.comms.core.TaskPlan;
+import edu.iu.dsc.tws.examples.Utils;
+import edu.iu.dsc.tws.examples.basic.batch.terasort.utils.DataLoader;
+import edu.iu.dsc.tws.examples.basic.batch.terasort.utils.Record;
 import edu.iu.dsc.tws.rsched.spi.container.IContainer;
 import edu.iu.dsc.tws.rsched.spi.resource.ResourcePlan;
 
@@ -19,8 +29,53 @@ import edu.iu.dsc.tws.rsched.spi.resource.ResourcePlan;
  * Example that performs tera sort
  */
 public class TeraSortContainer implements IContainer {
+  private static final Logger LOG = Logger.
+      getLogger(TeraSortContainer.class.getName());
+  private String inputFolder;
+  private String filePrefix;
+  private String outputFolder;
+
+  private int partitionSampleNodes;
+  private int partitionSamplesPerNode;
+
+  private int id;
+
+  private Config config;
+  private ResourcePlan resourcePlan;
+  private static final int NO_OF_TASKS = 24;
+
+  private int noOfTasksPerExecutor = 2;
+
   @Override
-  public void init(Config config, int id, ResourcePlan resourcePlan) {
+  public void init(Config cfg, int containerId, ResourcePlan plan) {
+
+    this.config = cfg;
+    this.id = containerId;
+    this.resourcePlan = plan;
+
+    this.noOfTasksPerExecutor = NO_OF_TASKS / plan.noOfContainers();
+    //Need to get this from the Config
+    inputFolder = "/scratch/pulasthi/terasort/input_test";
+    outputFolder = "/scratch/pulasthi/terasort/out_test";
+    partitionSampleNodes = 12;
+    partitionSamplesPerNode = 10000;
+    filePrefix = "part";
+
+    // lets create the task plan
+    TaskPlan taskPlan = Utils.createReduceTaskPlan(cfg, plan, NO_OF_TASKS);
+    //first get the communication config file
+    TWSNetwork network = new TWSNetwork(cfg, taskPlan);
+
+    TWSCommunication channel = network.getDataFlowTWSCommunication();
+    for (int i = 0; i < noOfTasksPerExecutor; i++) {
+      int taskId = i;
+      LOG.info("Local rank: " + i);
+      String inputFile = Paths.get(inputFolder, filePrefix
+          + id + "_" + Integer.toString(taskId)).toString();
+      String outputFile = Paths.get(outputFolder, filePrefix + Integer.toString(id)).toString();
+      List<Record> records = DataLoader.load(id, inputFile);
+
+    }
 
   }
 }
--- twister2/examples/src/java/edu/iu/dsc/tws/examples/basic/batch/terasort/TeraSortJob.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/basic/batch/terasort/TeraSortJob.java
@@ -24,6 +24,16 @@
 package edu.iu.dsc.tws.examples.basic.batch.terasort;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import edu.iu.dsc.tws.api.JobConfig;
 import edu.iu.dsc.tws.api.Twister2Submitter;
@@ -33,20 +43,49 @@ import edu.iu.dsc.tws.rsched.core.ResourceAllocator;
 import edu.iu.dsc.tws.rsched.spi.resource.ResourceContainer;
 
 public final class TeraSortJob {
+  private static final Logger LOG = Logger.getLogger(TeraSortJob.class.getName());
+
   private TeraSortJob() {
   }
 
   public static void main(String[] args) {
+    Options options = new Options();
+    options.addOption("input", true, "Input directory");
+    options.addOption("output", true, "Output directory");
+    options.addOption("partitionSampleNodes", true, "Number of nodes to choose partition samples");
+    options.addOption("partitionSamplesPerNode",
+        true, "Number of samples to choose from each node");
+    options.addOption("filePrefix", true, "Prefix of the file partition");
+    CommandLineParser commandLineParser = new DefaultParser();
+    CommandLine cmd;
+    try {
+      cmd = commandLineParser.parse(options, args);
+    } catch (ParseException e) {
+      LOG.log(Level.SEVERE, "Failed to read the options", e);
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("program", options);
+      throw new RuntimeException(e);
+    }
+
+    Map<String, Object> inputs = new HashMap<>();
+    inputs.put("input", cmd.getOptionValue("input"));
+    inputs.put("output", cmd.getOptionValue("output"));
+    inputs.put("partitionSampleNodes",
+        Integer.parseInt(cmd.getOptionValue("partitionSampleNodes")));
+    inputs.put("partitionSamplesPerNode",
+        Integer.parseInt(cmd.getOptionValue("partitionSamplesPerNode")));
+    inputs.put("filePrefix", cmd.getOptionValue("filePrefix"));
     // first load the configurations from command line and config files
-    Config config = ResourceAllocator.loadConfig(new HashMap<>());
+    Config config = ResourceAllocator.loadConfig(inputs);
 
     // build JobConfig
     JobConfig jobConfig = new JobConfig();
 
+
     BasicJob.BasicJobBuilder jobBuilder = BasicJob.newBuilder();
     jobBuilder.setName("terasort");
     jobBuilder.setContainerClass(TeraSortContainer.class.getName());
-    jobBuilder.setRequestResource(new ResourceContainer(2, 1024), 4);
+    jobBuilder.setRequestResource(new ResourceContainer(2, 1024), 12);
     jobBuilder.setConfig(jobConfig);
 
     // now submit the job
