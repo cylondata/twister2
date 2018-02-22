c57a6ca5d491c96cbe684f7b2889fb2256f4fb54
pulasthi <pulasthi911@gmail.com>
2018-02-22 16:27:00 -0500

    updating PartitionCommunication example

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/basic/comms/BasePartitionCommunication.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/basic/comms/BasePartitionCommunication.java
@@ -35,11 +35,13 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.examples.basic.comms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -111,8 +113,21 @@ public class BasePartitionCommunication implements IContainer {
     try {
       // this method calls the init method
       // I think this is wrong
-      partition = channel.partition(newCfg, MessageType.INTEGER, 0, sources,
-          dests, new FinalPartitionReciver());
+      Map<Integer, List<Integer>> expectedIds = new HashMap<>();
+      for (int i = 0; i < NO_OF_TASKS; i++) {
+        expectedIds.put(i, new ArrayList<>());
+        for (int j = 0; j < NO_OF_TASKS; j++) {
+          if (!(i == j)) {
+            expectedIds.get(i).add(j);
+
+          }
+        }
+      }
+      FinalPartitionReciver finalPartitionRec = new FinalPartitionReciver();
+      partition = channel.partition(newCfg, MessageType.INTEGER, 2, sources,
+          dests, finalPartitionRec);
+      finalPartitionRec.setMap(expectedIds);
+      // partition.setMemoryMapped(true);
 
       for (int i = 0; i < noOfTasksPerExecutor; i++) {
         // the map thread where data is produced
@@ -152,13 +167,13 @@ public class BasePartitionCommunication implements IContainer {
     public void run() {
       try {
         LOG.log(Level.INFO, "Starting map worker: " + id);
-//      MPIBuffer data = new MPIBuffer(1024);
         int[] data = {task, task * 100};
-        int dest = (task + 1) % NO_OF_TASKS;
-        for (int i = 0; i < 1; i++) {
-          // lets generate a message
+        for (int i = 0; i < NO_OF_TASKS; i++) {
+          if (i == task) {
+            continue;
+          }
           int flags = MessageFlags.FLAGS_LAST;
-          while (!partition.send(task, data, flags, dest)) {
+          while (!partition.send(task, data, flags, i)) {
             // lets wait a litte and try again
             try {
               Thread.sleep(1);
@@ -166,12 +181,6 @@ public class BasePartitionCommunication implements IContainer {
               e.printStackTrace();
             }
           }
-//          LOG.info(String.format("%d sending to %d", id, task)
-//              + " count: " + sendCount++);
-          if (i % 1000 == 0) {
-            LOG.info(String.format("%d sent %d", id, i));
-          }
-          Thread.yield();
         }
         LOG.info(String.format("%d Done sending", id));
         status = Status.MAP_FINISHED;
@@ -182,23 +191,55 @@ public class BasePartitionCommunication implements IContainer {
   }
 
   private class FinalPartitionReciver implements MessageReceiver {
+    private Map<Integer, Map<Integer, Boolean>> finished;
 
     private long start = System.nanoTime();
 
     @Override
     public void init(Config cfg, DataFlowOperation op, Map<Integer, List<Integer>> expectedIds) {
+      finished = new ConcurrentHashMap<>();
+      for (Integer integer : expectedIds.keySet()) {
+        Map<Integer, Boolean> perTarget = new ConcurrentHashMap<>();
+        for (Integer integer1 : expectedIds.get(integer)) {
+          perTarget.put(integer1, false);
+        }
+        finished.put(integer, perTarget);
+      }
     }
 
     @Override
     public boolean onMessage(int source, int path, int target, int flags, Object object) {
       // add the object to the map
-      System.out.printf("Dest Task %d got message from Task %d with value %d \n", target,
-          source, ((int[]) object)[1]);
+      if ((flags & MessageFlags.FLAGS_LAST) == MessageFlags.FLAGS_LAST) {
+        finished.get(target).put(source, true);
+      }
+
+      if (((flags & MessageFlags.FLAGS_LAST) == MessageFlags.FLAGS_LAST) && isAllFinished(target)) {
+        System.out.printf("All Done for Task %d \n", target);
+      }
       return true;
+    }
+
+    private boolean isAllFinished(int target) {
+      boolean isDone = true;
+      for (Boolean bol : finished.get(target).values()) {
+        isDone &= bol;
+      }
+      return isDone;
     }
 
     public void progress() {
 
+    }
+
+    public void setMap(Map<Integer, List<Integer>> expectedIds) {
+      for (Integer integer : expectedIds.keySet()) {
+        Map<Integer, Boolean> perTarget = new ConcurrentHashMap<>();
+        for (Integer integer1 : expectedIds.get(integer)) {
+          perTarget.put(integer1, false);
+        }
+        finished.put(integer, perTarget);
+      }
     }
   }
 
