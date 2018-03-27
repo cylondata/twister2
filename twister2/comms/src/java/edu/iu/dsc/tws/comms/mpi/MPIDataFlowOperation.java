c9287644f0b75ca02df5e340209174354cf31b4b
supunkamburugamuve <supun06@gmail.com>
2018-03-26 21:34:58 -0400

    fixing a bug on concurrent access to operations

--- twister2/comms/src/java/edu/iu/dsc/tws/comms/mpi/MPIDataFlowOperation.java
+++ twister2/comms/src/java/edu/iu/dsc/tws/comms/mpi/MPIDataFlowOperation.java
@@ -925,7 +925,8 @@ public class MPIDataFlowOperation implements MPIMessageListener, MPIMessageRelea
       opertionID = (int) System.currentTimeMillis();
       this.kryoSerializer = new KryoSerializer();
 //      Path dataPath = new Path("/scratch/pulasthi/terasort/lmdbdatabase_" + this.executor);
-      Path dataPath = new Path("/home/pulasthi/work/twister2/lmdbdatabase_" + this.executor);
+      Path dataPath = new Path(MPIContext.networkStoragePath(config)
+          + " /lmdbdatabase_" + this.executor);
       this.memoryManager = new LMDBMemoryManager(dataPath);
       if (!isKeyed) {
         this.operationMemoryManager = memoryManager.addOperation(opertionID,
