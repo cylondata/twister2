3fd27f690194697a729db50f501dd8c77ef10199
kgovind <kannan.gridlab@gmail.com>
2018-03-30 12:44:25 -0400

    Task Graph and Task Scheduling Integration Codes.

--- twister2/taskscheduler/src/java/edu/iu/dsc/tws/tsched/spi/taskschedule/Resource.java
+++ twister2/taskscheduler/src/java/edu/iu/dsc/tws/tsched/spi/taskschedule/Resource.java
@@ -11,8 +11,6 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.tsched.spi.taskschedule;
 
-import java.util.Objects;
-
 public class Resource {
 
   private Double ram;
@@ -29,7 +27,7 @@ public class Resource {
   public boolean equals(Object o) {
     if (o instanceof Resource) {
       Resource r = (Resource) o;
-      return (Objects.equals(this.getCpu(), r.getCpu()))
+      return (this.getCpu() == r.getCpu())
           && (this.getRam().equals(r.getRam()))
           && (this.getDisk().equals(r.getDisk()));
     } else {
