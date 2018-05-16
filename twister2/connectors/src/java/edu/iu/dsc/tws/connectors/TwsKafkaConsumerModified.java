18f0c0d9eedeb01704e5f1d133df528bd78eb187
Arunan Sugunakumar <arunans23@gmail.com>
2018-05-16 22:07:39 +0530

    create a modified kafka consumer to create with checkpoint manager

---
+++ twister2/connectors/src/java/edu/iu/dsc/tws/connectors/TwsKafkaConsumerModified.java
@@ -0,0 +1,119 @@
+
+package edu.iu.dsc.tws.connectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.connectors.config.KafkaConsumerConfig;
+import edu.iu.dsc.tws.master.CheckpointListener;
+import edu.iu.dsc.tws.task.api.SourceTask;
+import edu.iu.dsc.tws.task.api.TaskContext;
+import edu.iu.dsc.tws.task.api.streaming.CheckpointedFunction;
+
+public class TwsKafkaConsumerModified<T> extends SourceTask
+    implements CheckpointedFunction, CheckpointListener {
+  private static final long serialVersionUID = -264264120110286748L;
+  private static final Logger LOG = LoggerFactory.getLogger(TwsKafkaConsumer.class);
+
+  private Properties kafkaConfigs;
+  private Properties simpleKafkaConfig;
+  private List<TopicPartition> topicPartitions;
+  private int myIndex;
+  private int worldSize;
+  private TaskContext taskContext;
+  private Map<TopicPartition, OffsetAndMetadata> offsetsToCommit;
+  private List<KafkaTopicPartitionState> topicPartitionStates;
+
+  private boolean restoreState = false;
+  private volatile boolean consumerThreadStarted = false;
+
+  private  KafkaPartitionFinder partitionFinder;
+  private  KafkaTopicDescription topicDescription;
+  private  KafkaConsumerThread<T> kafkaConsumerThread;
+
+  @Override
+  public void run() {
+    if (!consumerThreadStarted) {
+      try {
+        kafkaConsumerThread.start();
+        LOG.info("Starting the consumer thread");
+      } catch (IllegalThreadStateException e) {
+        LOG.info("consumer is already started");
+      }
+    }
+  }
+
+  @Override
+  public void prepare(Config cfg, TaskContext context) {
+    this.myIndex = cfg.getIntegerValue("twister2.container.id", 0);
+    LOG.info("myID : {} , worldSize : {} ", myIndex, worldSize);
+    this.partitionFinder = new KafkaPartitionFinder(
+        simpleKafkaConfig, worldSize, myIndex, topicDescription);
+    this.topicPartitions = partitionFinder.getRelevantPartitions();
+
+    this.topicPartitionStates = new ArrayList<>();
+    for (TopicPartition tp : topicPartitions) {
+      topicPartitionStates.add(new KafkaTopicPartitionState(tp));
+    }
+
+    this.kafkaConsumerThread = new KafkaConsumerThread<T>(
+        kafkaConfigs, offsetsToCommit, topicPartitions, topicPartitionStates, context);
+    kafkaConsumerThread.assignPartitions();
+    LOG.info("{} partitions are assigned", this.topicPartitions.size());
+    kafkaConsumerThread.setSeekToBeginning();
+
+  }
+  public TwsKafkaConsumerModified(
+      List<String> topics,
+      List<String> servers,
+      String consumerGroup
+  ) {
+    this.topicDescription = new KafkaTopicDescription(topics);
+    this.kafkaConfigs = createKafkaConfig(servers, consumerGroup);
+    this.simpleKafkaConfig = createSimpleKafkaConfig(servers);
+  }
+
+  public TwsKafkaConsumerModified(
+      Pattern topicPattern,
+      List<String> servers,
+      String consumerGroup
+  ) {
+    this.topicDescription = new KafkaTopicDescription(topicPattern);
+    this.kafkaConfigs = createKafkaConfig(servers, consumerGroup);
+    this.simpleKafkaConfig = createSimpleKafkaConfig(servers);
+  }
+  public Properties getKafkaConfigs() {
+    return kafkaConfigs;
+  }
+
+  public void setKafkaConfigs(Properties kafkaConfigs) {
+    this.kafkaConfigs = kafkaConfigs;
+  }
+
+  private Properties createKafkaConfig(List<String> servers, String consumerGroup) {
+    return KafkaConsumerConfig.getStringDeserializerConfig(servers, consumerGroup);
+  }
+
+  private Properties createSimpleKafkaConfig(List<String> servers) {
+    return KafkaConsumerConfig.getSimpleKafkaConsumer(servers);
+  }
+
+  // temp method to pass worldSize since config file doesn't have the field //
+  public void setWorldSize(int worldSize) {
+    this.worldSize = worldSize;
+  }
+
+  @Override
+  public void notifyCheckpointComplete(long checkpointId) throws Exception {
+
+  }
+}
