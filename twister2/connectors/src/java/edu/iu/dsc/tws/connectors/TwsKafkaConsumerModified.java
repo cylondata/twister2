f752197aa4f48d9e0e39be4968ea3ecd4c9c3c3b
JMIsham <isham.14@cse.mrt.ac.lk>
2018-05-29 13:48:14 +0530

    build fix

--- twister2/connectors/src/java/edu/iu/dsc/tws/connectors/TwsKafkaConsumerModified.java
+++ twister2/connectors/src/java/edu/iu/dsc/tws/connectors/TwsKafkaConsumerModified.java
@@ -12,15 +12,15 @@ import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.iu.dsc.tws.checkpointmanager.CheckpointListener;
+//import edu.iu.dsc.tws.checkpointmanager.CheckpointListener;
 import edu.iu.dsc.tws.common.config.Config;
 import edu.iu.dsc.tws.connectors.config.KafkaConsumerConfig;
 import edu.iu.dsc.tws.task.api.SourceTask;
 import edu.iu.dsc.tws.task.api.TaskContext;
 import edu.iu.dsc.tws.task.api.streaming.CheckpointedFunction;
 
-public class TwsKafkaConsumerModified<T> extends SourceTask
-    implements CheckpointedFunction, CheckpointListener {
+public class TwsKafkaConsumerModified<T> extends SourceTask {
+//    implements CheckpointedFunction, CheckpointListener
   private static final long serialVersionUID = -264264120110286748L;
   private static final Logger LOG = LoggerFactory.getLogger(TwsKafkaConsumer.class);
 
@@ -113,7 +113,7 @@ public class TwsKafkaConsumerModified<T> extends SourceTask
     return KafkaConsumerConfig.getSimpleKafkaConsumer(servers);
   }
 
-  @Override
+//  @Override
   public void notifyCheckpointComplete(long checkpointId) throws Exception {
 
   }
