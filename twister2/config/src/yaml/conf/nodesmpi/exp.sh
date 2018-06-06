734cae4105890954685675ae50b273a036e5fee7
supunkamburugamuva <supun06@gmail.com>
2018-06-05 23:43:58 -0400

    fixing an error in compilation

--- twister2/config/src/yaml/conf/nodesmpi/exp.sh
+++ twister2/config/src/yaml/conf/nodesmpi/exp.sh
@@ -12,6 +12,6 @@ if [ $OMPI_COMM_WORLD_RANK = "0" ]; then
 fi
 
 profile=
-debug=
+#debug=
 
 java $debug $profile -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djava.util.logging.config.file=nodesmpi/logger.properties $cp -cp $3 edu.iu.dsc.tws.rsched.schedulers.mpi.MPIProcess --container_class $4 --job_name $5 --twister2_home $6 --cluster_type nodesmpi --config_dir $7 &
