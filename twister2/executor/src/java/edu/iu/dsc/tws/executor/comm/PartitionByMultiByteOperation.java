a788316abb7fd68699a957ee13bba46bc7baf16a
Vibhatha Abeykoon <vibhatha@gmail.com>
2018-06-15 09:40:53 -0400

    parition by multi byte example added

---
+++ twister2/executor/src/java/edu/iu/dsc/tws/executor/comm/PartitionByMultiByteOperation.java
@@ -0,0 +1,48 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.executor.comm;
+
+import java.util.concurrent.BlockingQueue;
+
+import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.comms.api.TWSChannel;
+import edu.iu.dsc.tws.comms.core.TaskPlan;
+import edu.iu.dsc.tws.task.api.IMessage;
+
+public class PartitionByMultiByteOperation extends AbstractParallelOperation {
+
+
+
+  public PartitionByMultiByteOperation(Config config, TWSChannel network, TaskPlan tPlan) {
+    super(config, network, tPlan);
+  }
+
+  @Override
+  public void send(int source, IMessage message) {
+    super.send(source, message);
+  }
+
+  @Override
+  public void send(int source, IMessage message, int dest) {
+    super.send(source, message, dest);
+  }
+
+  @Override
+  public void register(int targetTask, BlockingQueue<IMessage> queue) {
+    super.register(targetTask, queue);
+  }
+
+  @Override
+  public void progress() {
+    super.progress();
+  }
+}
