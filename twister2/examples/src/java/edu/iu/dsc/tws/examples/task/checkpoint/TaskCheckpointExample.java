0cdecd678d95e12d8a22da1a37a4afb435d85861
Arunan Sugunakumar <arunans23@gmail.com>
2018-06-17 21:20:17 +0530

    renew checkpoint example

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/task/checkpoint/TaskCheckpointExample.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/task/checkpoint/TaskCheckpointExample.java
@@ -35,6 +35,8 @@ import edu.iu.dsc.tws.common.config.Config;
 import edu.iu.dsc.tws.comms.core.TWSNetwork;
 import edu.iu.dsc.tws.executor.ExecutionPlan;
 import edu.iu.dsc.tws.executor.ExecutionPlanBuilder;
+import edu.iu.dsc.tws.executor.threading.ExecutionModel;
+import edu.iu.dsc.tws.executor.threading.ThreadExecutor;
 import edu.iu.dsc.tws.rsched.core.ResourceAllocator;
 import edu.iu.dsc.tws.rsched.spi.container.IContainer;
 import edu.iu.dsc.tws.rsched.spi.resource.ResourceContainer;
@@ -46,6 +48,7 @@ import edu.iu.dsc.tws.task.api.SourceTask;
 import edu.iu.dsc.tws.task.api.TaskContext;
 import edu.iu.dsc.tws.task.graph.DataFlowTaskGraph;
 import edu.iu.dsc.tws.task.graph.GraphBuilder;
+import edu.iu.dsc.tws.task.graph.GraphConstants;
 import edu.iu.dsc.tws.tsched.roundrobin.RoundRobinTaskScheduling;
 import edu.iu.dsc.tws.tsched.spi.scheduler.Worker;
 import edu.iu.dsc.tws.tsched.spi.scheduler.WorkerPlan;
@@ -64,6 +67,16 @@ public class TaskCheckpointExample implements IContainer {
     builder.setParallelism("sink", 4);
     builder.connect("source", "sink", "partition-edge", Operations.PARTITION);
 
+    builder.addConfiguration("source", "Ram", GraphConstants.taskInstanceRam(config));
+    builder.addConfiguration("source", "Disk", GraphConstants.taskInstanceDisk(config));
+    builder.addConfiguration("source", "Cpu", GraphConstants.taskInstanceCpu(config));
+
+    List<String> sourceInputDataset = new ArrayList<>();
+    sourceInputDataset.add("dataset1.txt");
+    sourceInputDataset.add("dataset2.txt");
+
+    builder.addConfiguration("source", "inputdataset", sourceInputDataset);
+
     DataFlowTaskGraph graph = builder.build();
 
     RoundRobinTaskScheduling roundRobinTaskScheduling = new RoundRobinTaskScheduling();
@@ -75,6 +88,9 @@ public class TaskCheckpointExample implements IContainer {
     TWSNetwork network = new TWSNetwork(config, resourcePlan.getThisId());
     ExecutionPlanBuilder executionPlanBuilder = new ExecutionPlanBuilder(resourcePlan, network);
     ExecutionPlan plan = executionPlanBuilder.schedule(config, graph, taskSchedulePlan);
+    ExecutionModel executionModel = new ExecutionModel(ExecutionModel.SHARED);
+    ThreadExecutor executor = new ThreadExecutor(executionModel, plan);
+    executor.execute();
 
     // we need to progress the channel
     while (true) {
@@ -113,14 +129,8 @@ public class TaskCheckpointExample implements IContainer {
     @Override
     public void execute(IMessage message) {
 
-      @SuppressWarnings("unchecked")
-      ArrayList<CheckpointBarrier> checkpointBarriers
-          = (ArrayList<CheckpointBarrier>) message.getContent();
-
-      System.out.println("Got an array with size " + checkpointBarriers.size());
-      for (int i = 0; i < checkpointBarriers.size(); i++) {
-        System.out.println("Got checkpoint barrier with id : " + checkpointBarriers.get(i).getId());
-      }
+      CheckpointBarrier cb = (CheckpointBarrier) message.getContent();
+      System.out.println(cb.getId());
     }
 
     @Override
