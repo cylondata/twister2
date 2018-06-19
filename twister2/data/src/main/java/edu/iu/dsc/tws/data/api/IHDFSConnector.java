aed00a0cd55138fac45f08b9e1d01726efa63941
kgovind <kannan.gridlab@gmail.com>
2018-06-19 16:57:46 -0400

    Twister2 -> HDFS Integration initial code

---
+++ twister2/data/src/main/java/edu/iu/dsc/tws/data/api/IHDFSConnector.java
@@ -0,0 +1,37 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.data.api;
+
+import edu.iu.dsc.tws.data.hdfs.HadoopFileSystem;
+
+/**
+ * This interface hides the configuration and connection details of the Hadoop File System.
+ */
+public interface IHDFSConnector {
+
+  HadoopFileSystem HDFSConnect(String message, int count);
+
+  void HDFSConnect(String message);
+
+}
