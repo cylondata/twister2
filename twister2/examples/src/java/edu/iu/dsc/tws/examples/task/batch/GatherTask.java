41b14b4d12ec6d9063e9202b99ffb09df1161791
Vibhatha Abeykoon <vibhatha@gmail.com>
2018-07-12 11:57:24 -0400

    streaming and batch examples added.

---
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/task/batch/GatherTask.java
@@ -0,0 +1,172 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.examples.task.streaming;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import edu.iu.dsc.tws.api.JobConfig;
+import edu.iu.dsc.tws.api.Twister2Submitter;
+import edu.iu.dsc.tws.api.basic.job.BasicJob;
+import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.comms.api.MessageType;
+import edu.iu.dsc.tws.comms.core.TWSNetwork;
+import edu.iu.dsc.tws.comms.dfw.io.KeyedContent;
+import edu.iu.dsc.tws.examples.utils.RandomString;
+import edu.iu.dsc.tws.executor.ExecutionPlan;
+import edu.iu.dsc.tws.executor.ExecutionPlanBuilder;
+import edu.iu.dsc.tws.executor.threading.ExecutionModel;
+import edu.iu.dsc.tws.executor.threading.ThreadExecutor;
+import edu.iu.dsc.tws.rsched.core.ResourceAllocator;
+import edu.iu.dsc.tws.rsched.core.SchedulerContext;
+import edu.iu.dsc.tws.rsched.spi.container.IContainer;
+import edu.iu.dsc.tws.rsched.spi.resource.ResourceContainer;
+import edu.iu.dsc.tws.rsched.spi.resource.ResourcePlan;
+import edu.iu.dsc.tws.task.api.IMessage;
+import edu.iu.dsc.tws.task.api.Operations;
+import edu.iu.dsc.tws.task.api.SinkTask;
+import edu.iu.dsc.tws.task.api.SourceTask;
+import edu.iu.dsc.tws.task.api.TaskContext;
+import edu.iu.dsc.tws.task.graph.DataFlowTaskGraph;
+import edu.iu.dsc.tws.task.graph.GraphBuilder;
+import edu.iu.dsc.tws.tsched.roundrobin.RoundRobinTaskScheduling;
+import edu.iu.dsc.tws.tsched.spi.scheduler.Worker;
+import edu.iu.dsc.tws.tsched.spi.scheduler.WorkerPlan;
+import edu.iu.dsc.tws.tsched.spi.taskschedule.TaskSchedulePlan;
+
+public class GatherTask implements IContainer {
+
+  private RandomString randomString;
+
+  @Override
+  public void init(Config config, int id, ResourcePlan resourcePlan) {
+    GeneratorTask g = new GeneratorTask();
+    RecevingTask r = new RecevingTask();
+
+    GraphBuilder builder = GraphBuilder.newBuilder();
+    builder.addSource("source", g);
+    builder.setParallelism("source", 4);
+    builder.addSink("sink", r);
+    builder.setParallelism("sink", 1);
+    builder.connect("source", "sink", "gather-edge", Operations.GATHER);
+
+
+    DataFlowTaskGraph graph = builder.build();
+    RoundRobinTaskScheduling roundRobinTaskScheduling = new RoundRobinTaskScheduling();
+    roundRobinTaskScheduling.initialize(config);
+
+    WorkerPlan workerPlan = createWorkerPlan(resourcePlan);
+    TaskSchedulePlan taskSchedulePlan = roundRobinTaskScheduling.schedule(graph, workerPlan);
+
+    TWSNetwork network = new TWSNetwork(config, resourcePlan.getThisId());
+    ExecutionPlanBuilder executionPlanBuilder = new ExecutionPlanBuilder(resourcePlan, network);
+    ExecutionPlan plan = executionPlanBuilder.schedule(config, graph, taskSchedulePlan);
+    ExecutionModel executionModel = new ExecutionModel(ExecutionModel.SHARED);
+    ThreadExecutor executor = new ThreadExecutor(executionModel, plan, network.getChannel());
+    executor.execute();
+  }
+
+  private static class GeneratorTask extends SourceTask {
+    private static final long serialVersionUID = -254264903510284748L;
+    private TaskContext ctx;
+    private Config config;
+    private static RandomString randomString;
+
+    @Override
+    public void run() {
+      randomString = new RandomString(128000, new Random(), RandomString.ALPHANUM);
+      String data = generateStringData();
+      // lets generate a message
+      KeyedContent message = new KeyedContent(0, data,
+          MessageType.INTEGER, MessageType.OBJECT);
+//      System.out.println("Message : Key :" + message.getKey().toString() + ", Value : "
+//          + message.getValue());
+      ctx.write("gather-edge", "1");
+    }
+
+    @Override
+    public void prepare(Config cfg, TaskContext context) {
+      this.ctx = context;
+    }
+
+    private static String generateStringData() {
+      return "1";
+    }
+  }
+
+  private static class RecevingTask extends SinkTask {
+    private int count = 0;
+    private static final long serialVersionUID = -254264903510284798L;
+
+    @Override
+    public void execute(IMessage message) {
+      if (count % 100000 == 0) {
+        System.out.println("Message Gathered : " + message.getContent() + ", Count : " + count);
+      }
+      count++;
+    }
+
+    @Override
+    public void prepare(Config cfg, TaskContext context) {
+
+    }
+  }
+
+  public WorkerPlan createWorkerPlan(ResourcePlan resourcePlan) {
+    List<Worker> workers = new ArrayList<>();
+    for (ResourceContainer resource : resourcePlan.getContainers()) {
+      Worker w = new Worker(resource.getId());
+      workers.add(w);
+    }
+
+    return new WorkerPlan(workers);
+  }
+
+
+  public static void main(String[] args) {
+    // first load the configurations from command line and config files
+    Config config = ResourceAllocator.loadConfig(new HashMap<>());
+
+    // build JobConfig
+    HashMap<String, Object> configurations = new HashMap<>();
+    configurations.put(SchedulerContext.THREADS_PER_WORKER, 8);
+
+    // build JobConfig
+    JobConfig jobConfig = new JobConfig();
+    jobConfig.putAll(configurations);
+
+    BasicJob.BasicJobBuilder jobBuilder = BasicJob.newBuilder();
+    jobBuilder.setName("task-gather");
+    jobBuilder.setContainerClass(GatherTask.class.getName());
+    jobBuilder.setRequestResource(new ResourceContainer(4, 1024), 4);
+    jobBuilder.setConfig(jobConfig);
+
+    // now submit the job
+    Twister2Submitter.submitContainerJob(jobBuilder.build(), config);
+  }
+}
+
