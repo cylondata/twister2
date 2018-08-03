cfa66b6596df16092de62705215789b2868bfa20
supunkamburugamuva <supun06@gmail.com>
2018-08-03 16:28:01 -0400

    adding gather

---
+++ twister2/comms/src/java/edu/iu/dsc/tws/comms/op/stream/SGather.java
@@ -0,0 +1,59 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.comms.op.stream;
+
+import java.util.Set;
+import java.util.logging.Logger;
+
+import edu.iu.dsc.tws.comms.api.BatchReceiver;
+import edu.iu.dsc.tws.comms.api.MessageType;
+import edu.iu.dsc.tws.comms.core.TaskPlan;
+import edu.iu.dsc.tws.comms.dfw.DataFlowGather;
+import edu.iu.dsc.tws.comms.dfw.io.gather.StreamingFinalGatherReceiver;
+import edu.iu.dsc.tws.comms.dfw.io.gather.StreamingPartialGatherReceiver;
+import edu.iu.dsc.tws.comms.op.Communicator;
+
+public class SGather {
+  private static final Logger LOG = Logger.getLogger(SReduce.class.getName());
+
+  private DataFlowGather gather;
+
+  public SGather(Communicator comm, TaskPlan plan,
+                 Set<Integer> sources, int destination,
+                 BatchReceiver rcvr, MessageType dataType) {
+    gather = new DataFlowGather(comm.getChannel(), sources, destination,
+        new StreamingFinalGatherReceiver(rcvr),
+        new StreamingPartialGatherReceiver(), 0, 0,
+        comm.getConfig(), dataType, plan, comm.nextEdge());
+    gather.init(comm.getConfig(), dataType, plan, comm.nextEdge());
+  }
+
+  public boolean reduce(int src, Object message, int flags) {
+    return gather.send(src, message, flags);
+  }
+
+  public boolean hasPending() {
+    return !gather.isComplete();
+  }
+
+  public boolean progress() {
+    return gather.progress();
+  }
+
+  public void close() {
+    // deregister from the channel
+  }
+
+  public void finish(int src) {
+    gather.finish(src);
+  }
+}
