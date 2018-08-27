c94626ac7e63e841a3268567bfa8a22477e76d58
Ahmet Uyar <ahmetuyar@gmail.com>
2018-08-27 17:06:34 +0300

    exceptions and interfaces packages under rsched

--- twister2/resource-scheduler/src/java/edu/iu/dsc/tws/rsched/exceptions/UploaderException.java
+++ twister2/resource-scheduler/src/java/edu/iu/dsc/tws/rsched/exceptions/UploaderException.java
@@ -1,5 +1,3 @@
-//  Copyright 2017 Twitter. All rights reserved.
-//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
@@ -11,7 +9,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-package edu.iu.dsc.tws.rsched.spi.uploaders;
+package edu.iu.dsc.tws.rsched.exceptions;
 
 /**
  * Thrown to indicate that an error occurred while uploading package
