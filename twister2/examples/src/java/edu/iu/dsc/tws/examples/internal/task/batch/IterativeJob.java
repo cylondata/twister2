42669bfe0a79c415c48bbf65d4e766779054f195
supunkamburugamuva <supun06@gmail.com>
2018-08-30 12:50:53 -0400

    iterative job

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/internal/task/batch/IterativeJob.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/internal/task/batch/IterativeJob.java
@@ -11,12 +11,20 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.examples.internal.task.batch;
 
+import java.util.HashMap;
+
+import edu.iu.dsc.tws.api.JobConfig;
+import edu.iu.dsc.tws.api.Twister2Submitter;
+import edu.iu.dsc.tws.api.job.Twister2Job;
 import edu.iu.dsc.tws.api.task.TaskWorker;
 import edu.iu.dsc.tws.common.config.Config;
 import edu.iu.dsc.tws.common.discovery.IWorkerController;
 import edu.iu.dsc.tws.common.resource.AllocatedResources;
+import edu.iu.dsc.tws.common.resource.WorkerComputeResource;
 import edu.iu.dsc.tws.common.worker.IPersistentVolume;
 import edu.iu.dsc.tws.common.worker.IVolatileVolume;
+import edu.iu.dsc.tws.rsched.core.ResourceAllocator;
+import edu.iu.dsc.tws.rsched.core.SchedulerContext;
 
 public class IterativeJob extends TaskWorker {
   @Override
@@ -24,5 +32,27 @@ public class IterativeJob extends TaskWorker {
                       IWorkerController workerController, IPersistentVolume persistentVolume,
                       IVolatileVolume volatileVolume) {
 
+  }
+
+  public static void main(String[] args) {
+    // first load the configurations from command line and config files
+    Config config = ResourceAllocator.loadConfig(new HashMap<>());
+
+    // build JobConfig
+    HashMap<String, Object> configurations = new HashMap<>();
+    configurations.put(SchedulerContext.THREADS_PER_WORKER, 8);
+
+    // build JobConfig
+    JobConfig jobConfig = new JobConfig();
+    jobConfig.putAll(configurations);
+
+    Twister2Job.BasicJobBuilder jobBuilder = Twister2Job.newBuilder();
+    jobBuilder.setName("iterative-job");
+    jobBuilder.setWorkerClass(IterativeJob.class.getName());
+    jobBuilder.setRequestResource(new WorkerComputeResource(4, 1024), 4);
+    jobBuilder.setConfig(jobConfig);
+
+    // now submit the job
+    Twister2Submitter.submitJob(jobBuilder.build(), config);
   }
 }
