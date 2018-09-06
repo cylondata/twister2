969c6210144ab8b88c8fbbb698a71fc28f366e86
kannang83 <kannan.gridlab@gmail.com>
2018-09-06 16:15:48 -0400

    Fix for compilation issue.

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/internal/hdfs/HDFSTaskExample.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/internal/hdfs/HDFSTaskExample.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.iu.dsc.tws.api.JobConfig;
