d79fc605e7453fe04f256995cb53fede70912266
JMIsham <isham.14@cse.mrt.ac.lk>
2018-09-21 12:23:28 +0530

    re-writing kafka example

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/internal/task/streaming/ReduceKafkaStreamingTask.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/internal/task/streaming/ReduceKafkaStreamingTask.java
@@ -9,23 +9,12 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
 package edu.iu.dsc.tws.examples.internal.task.streaming;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 import edu.iu.dsc.tws.api.JobConfig;
 import edu.iu.dsc.tws.api.Twister2Submitter;
@@ -40,7 +29,6 @@ import edu.iu.dsc.tws.common.worker.IWorker;
 import edu.iu.dsc.tws.connectors.TwsKafkaConsumer;
 import edu.iu.dsc.tws.connectors.TwsKafkaProducer;
 import edu.iu.dsc.tws.examples.internal.task.TaskUtils;
-
 import edu.iu.dsc.tws.executor.core.OperationNames;
 import edu.iu.dsc.tws.rsched.core.ResourceAllocator;
 import edu.iu.dsc.tws.rsched.core.SchedulerContext;
@@ -55,11 +43,14 @@ import edu.iu.dsc.tws.tsched.spi.scheduler.Worker;
 import edu.iu.dsc.tws.tsched.spi.scheduler.WorkerPlan;
 
 public class ReduceKafkaStreamingTask implements IWorker {
+  private static final Logger LOG = Logger.getLogger(ReduceStreamingTask.class.getName());
+
   @Override
   public void execute(Config config, int workerID, AllocatedResources resources,
                       IWorkerController workerController,
                       IPersistentVolume persistentVolume,
                       IVolatileVolume volatileVolume) {
+
     List<String> topics = new ArrayList<>();
     topics.add("sample_topic1");
     List<String> servers = new ArrayList<>();
@@ -69,7 +60,6 @@ public class ReduceKafkaStreamingTask implements IWorker {
         servers,
         "test",
         "reduce-edge");
-    RecevingTask r2 = new RecevingTask();
     TwsKafkaProducer<String> r = new TwsKafkaProducer<String>(
         "outTopic",
         servers
@@ -81,45 +71,17 @@ public class ReduceKafkaStreamingTask implements IWorker {
     builder.addSink("sink", r);
     builder.setParallelism("sink", 1);
     builder.connect("source", "sink", "reduce-edge",
-        OperationNames.REDUCE);
+        OperationNames.REDUCE, new IFunction() {
+          @Override
+          public Object onMessage(Object object1, Object object2) {
+            return object1;
+          }
+        });
     builder.operationMode(OperationMode.STREAMING);
-
     DataFlowTaskGraph graph = builder.build();
+
     TaskUtils.execute(config, resources, graph, workerController);
   }
-
-  private static class GeneratorTask extends BaseStreamSource {
-    private static final long serialVersionUID = -254264903510284748L;
-
-    @Override
-    public void execute() {
-      context.write("reduce-edge", "Hello");
-    }
-  }
-
-  private static class RecevingTask extends BaseStreamSink {
-    private static final long serialVersionUID = -254264903510284798L;
-    private int count = 0;
-
-    @Override
-    public boolean execute(IMessage message) {
-      if (count % 1000000 == 0) {
-        System.out.println("Message Reduced : " + message.getContent() + ", Count : " + count);
-      }
-      count++;
-      return true;
-    }
-  }
-
-  public static class IdentityFunction implements IFunction {
-    private static final long serialVersionUID = -254264903510284748L;
-
-    @Override
-    public Object onMessage(Object object1, Object object2) {
-      return object1;
-    }
-  }
-
 
   public WorkerPlan createWorkerPlan(AllocatedResources resourcePlan) {
     List<Worker> workers = new ArrayList<>();
@@ -134,8 +96,6 @@ public class ReduceKafkaStreamingTask implements IWorker {
   public static void main(String[] args) {
     // first load the configurations from command line and config files
     Config config = ResourceAllocator.loadConfig(new HashMap<>());
-
-    // build JobConfig
     HashMap<String, Object> configurations = new HashMap<>();
     configurations.put(SchedulerContext.THREADS_PER_WORKER, 8);
 
@@ -144,8 +104,8 @@ public class ReduceKafkaStreamingTask implements IWorker {
     jobConfig.putAll(configurations);
 
     Twister2Job.BasicJobBuilder jobBuilder = Twister2Job.newBuilder();
-    jobBuilder.setName("reduce-task");
-    jobBuilder.setWorkerClass(ReduceKafkaStreamingTask.class.getName());
+    jobBuilder.setName("reduce-kafka-task");
+    jobBuilder.setWorkerClass(ReduceStreamingTask.class.getName());
     jobBuilder.setRequestResource(new WorkerComputeResource(2, 1024), 4);
     jobBuilder.setConfig(jobConfig);
 
