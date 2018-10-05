f9ade339fa0da21a908a6a0ed4daa154f6df0ae5
pulasthi <pulasthi911@gmail.com>
2018-10-04 23:02:44 -0400

    adding stream keyed gather example

---
+++ twister2/comms/src/java/edu/iu/dsc/tws/comms/op/stream/SKeyedGather.java
@@ -0,0 +1,126 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package edu.iu.dsc.tws.comms.op.stream;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.iu.dsc.tws.comms.api.BulkReceiver;
+import edu.iu.dsc.tws.comms.api.DestinationSelector;
+import edu.iu.dsc.tws.comms.api.MessageType;
+import edu.iu.dsc.tws.comms.api.ReduceFunction;
+import edu.iu.dsc.tws.comms.core.TaskPlan;
+import edu.iu.dsc.tws.comms.dfw.DataFlowMultiGather;
+import edu.iu.dsc.tws.comms.dfw.io.KeyedContent;
+import edu.iu.dsc.tws.comms.dfw.io.gather.GatherMultiStreamingFinalReceiver;
+import edu.iu.dsc.tws.comms.dfw.io.gather.GatherMultiStreamingPartialReceiver;
+import edu.iu.dsc.tws.comms.op.Communicator;
+
+/**
+ * Streaming Keyed Partition Operation
+ */
+public class SKeyedGather {
+  /**
+   * The actual operation
+   */
+  private DataFlowMultiGather keyedGather;
+
+  /**
+   * Destination selector
+   */
+  private DestinationSelector destinationSelector;
+
+  /**
+   * Key type
+   */
+  private MessageType keyType;
+
+  /**
+   * Data type
+   */
+  private MessageType dataType;
+
+  /**
+   * Construct a Streaming Key based partition operation
+   *
+   * @param comm the communicator
+   * @param plan task plan
+   * @param sources source tasks
+   * @param targets target tasks
+   * @param dType data type
+   * @param kType key type
+   * @param rcvr receiver
+   * @param destSelector destination selector
+   */
+  public SKeyedGather(Communicator comm, TaskPlan plan,
+                      Set<Integer> sources, Set<Integer> targets, MessageType kType,
+                      MessageType dType, BulkReceiver rcvr,
+                      DestinationSelector destSelector) {
+    this.keyType = kType;
+    this.dataType = dType;
+
+    Set<Integer> edges = new HashSet<>();
+    for (int i = 0; i < targets.size(); i++) {
+      edges.add(comm.nextEdge());
+    }
+
+    this.keyedGather = new DataFlowMultiGather(comm.getChannel(), sources, targets,
+        new GatherMultiStreamingFinalReceiver(rcvr),
+        new GatherMultiStreamingPartialReceiver(), edges, keyType, dataType);
+    this.keyedGather.init(comm.getConfig(), dataType, plan);
+    this.destinationSelector = destSelector;
+    this.destinationSelector.prepare(keyType, sources, targets);
+
+  }
+
+  /**
+   * Send a message to be reduced
+   *
+   * @param src source
+   * @param key key
+   * @param message message
+   * @param flags message flag
+   * @return true if the message is accepted
+   */
+  public boolean reduce(int src, Object key, Object message, int flags) {
+    int dest = destinationSelector.next(src, key, message);
+    return keyedGather.send(src, new KeyedContent(key, message, keyType, dataType), flags, dest);
+  }
+
+  /**
+   * Weather we have messages pending
+   *
+   * @return true if there are messages pending
+   */
+  public boolean hasPending() {
+    return !keyedGather.isComplete();
+  }
+
+  /**
+   * Indicate the end of the communication
+   *
+   * @param src the source that is ending
+   */
+  public void finish(int src) {
+    keyedGather.finish(src);
+  }
+
+  /**
+   * Progress the operation, if not called, messages will not be processed
+   *
+   * @return true if further progress is needed
+   */
+  public boolean progress() {
+    return keyedGather.progress();
+  }
+}
---
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/comms/stream/SKeyedGatherExample.java
@@ -0,0 +1,134 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.examples.comms.stream;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.common.collect.Iterators;
+
+import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.comms.api.BulkReceiver;
+import edu.iu.dsc.tws.comms.api.MessageType;
+import edu.iu.dsc.tws.comms.api.Op;
+import edu.iu.dsc.tws.comms.core.TaskPlan;
+import edu.iu.dsc.tws.comms.op.functions.reduction.ReduceOperationFunction;
+import edu.iu.dsc.tws.comms.op.selectors.SimpleKeyBasedSelector;
+import edu.iu.dsc.tws.comms.op.stream.SKeyedGather;
+import edu.iu.dsc.tws.examples.Utils;
+import edu.iu.dsc.tws.examples.comms.KeyedBenchWorker;
+import edu.iu.dsc.tws.examples.verification.ExperimentVerification;
+import edu.iu.dsc.tws.examples.verification.VerificationException;
+import edu.iu.dsc.tws.executor.core.OperationNames;
+
+public class SKeyedGatherExample extends KeyedBenchWorker {
+  private static final Logger LOG = Logger.getLogger(SKeyedGatherExample.class.getName());
+
+  private SKeyedGather keyedGather;
+
+  private boolean reduceDone;
+
+  @Override
+  protected void execute() {
+    TaskPlan taskPlan = Utils.createStageTaskPlan(config, resourcePlan,
+        jobParameters.getTaskStages(), workerList);
+
+    Set<Integer> sources = new HashSet<>();
+    Integer noOfSourceTasks = jobParameters.getTaskStages().get(0);
+    for (int i = 0; i < noOfSourceTasks; i++) {
+      sources.add(i);
+    }
+    Set<Integer> targets = new HashSet<>();
+    Integer noOfTargetTasks = jobParameters.getTaskStages().get(1);
+    for (int i = 0; i < noOfTargetTasks; i++) {
+      targets.add(noOfSourceTasks + i);
+    }
+
+    keyedGather = new SKeyedGather(communicator, taskPlan, sources, targets,
+        MessageType.OBJECT, MessageType.OBJECT,
+        new GatherBulkReceiver(), new SimpleKeyBasedSelector());
+
+    Set<Integer> tasksOfExecutor = Utils.getTasksOfExecutor(workerId, taskPlan,
+        jobParameters.getTaskStages(), 0);
+    for (int t : tasksOfExecutor) {
+      finishedSources.put(t, false);
+    }
+    if (tasksOfExecutor.size() == 0) {
+      sourcesDone = true;
+    }
+
+    LOG.log(Level.INFO, String.format("%d Sources %s target %d this %s",
+        workerId, sources, 1, tasksOfExecutor));
+    // now initialize the workers
+    for (int t : tasksOfExecutor) {
+      // the map thread where data is produced
+      Thread mapThread = new Thread(new MapWorker(t));
+      mapThread.start();
+    }
+
+  }
+
+  @Override
+  protected void progressCommunication() {
+    keyedGather.progress();
+  }
+
+  @Override
+  protected boolean isDone() {
+    return reduceDone && sourcesDone && !keyedGather.hasPending();
+  }
+
+  @Override
+  protected boolean sendMessages(int task, Object key, Object data, int flag) {
+    while (!keyedGather.reduce(task, key, data, flag)) {
+      // lets wait a litte and try again
+      keyedGather.progress();
+    }
+    return true;
+  }
+
+  public class GatherBulkReceiver implements BulkReceiver {
+    private int count = 0;
+    private int expected;
+
+    @Override
+    public void init(Config cfg, Set<Integer> expectedIds) {
+      expected = expectedIds.size() * jobParameters.getIterations();
+    }
+
+    @Override
+    public boolean receive(int target, Iterator<Object> it) {
+      LOG.log(Level.INFO, String.format("%d Received message %d count %d",
+          workerId, target, Iterators.size(it)));
+      return true;
+    }
+  }
+
+  public void verify() throws VerificationException {
+    boolean doVerify = jobParameters.isDoVerify();
+    boolean isVerified = false;
+    if (doVerify) {
+      LOG.info("Verifying results ...");
+      ExperimentVerification experimentVerification
+          = new ExperimentVerification(experimentData, OperationNames.KEYED_REDUCE);
+      isVerified = experimentVerification.isVerified();
+      if (isVerified) {
+        LOG.info("Results generated from the experiment are verified.");
+      } else {
+        throw new VerificationException("Results do not match");
+      }
+    }
+  }
+}
