fa74568adbcc027a21c580f5b255206214f2f9c0
JMIsham <isham.14@cse.mrt.ac.lk>
2018-10-12 12:11:22 +0530

    adding time interval logic to SourceCheckpointableTask

--- twister2/task/src/main/java/edu/iu/dsc/tws/task/api/SourceCheckpointableTask.java
+++ twister2/task/src/main/java/edu/iu/dsc/tws/task/api/SourceCheckpointableTask.java
@@ -25,7 +25,7 @@ import edu.iu.dsc.tws.common.net.tcp.request.RRClient;
 import edu.iu.dsc.tws.common.net.tcp.request.RequestID;
 import edu.iu.dsc.tws.proto.checkpoint.Checkpoint;
 import edu.iu.dsc.tws.task.streaming.BaseStreamSource;
-import static java.lang.Math.max;
+//import static java.lang.Math.max;
 
 public abstract class SourceCheckpointableTask extends BaseStreamSource {
   private static final long serialVersionUID = -254264903510214728L;
@@ -40,10 +40,12 @@ public abstract class SourceCheckpointableTask extends BaseStreamSource {
 
   private int currentBarrierID = 1;
   private int intermediateMessageCount;
-  private int currentCheckpointInterval = 50;
-  private int globalCheckpointInterval = 50;
+  private long currentCheckpointInterval = 5000;
+  private long globalCheckpointInterval = 5000;
   private long overallMessageCount = 0;
-
+  private final long startedTime = System.currentTimeMillis();
+  private long lastCheckpointTime;
+  private boolean responsePending;
   /**
    * to control the connection error when we repeatedly try connecting
    */
@@ -53,6 +55,7 @@ public abstract class SourceCheckpointableTask extends BaseStreamSource {
     this.ctx = context;
     this.config = cfg;
     this.intermediateMessageCount = 0;
+    this.lastCheckpointTime = startedTime;
     taskLooper = new Progress();
 
     taskClient = new RRClient("localhost", 6789, cfg, taskLooper,
@@ -83,16 +86,24 @@ public abstract class SourceCheckpointableTask extends BaseStreamSource {
               " currentCheckpointInterval: " + currentCheckpointInterval
       );
       checkForBarrier();
-      intermediateMessageCount = 0;
-      currentCheckpointInterval = max(
-          (int) (currentCheckpointInterval - currentCheckpointInterval * 0.5),
-          1);
     }
 
   }
 
+  public void checkForTimeInterval() {
+    if (getResponsePending()) {
+      currentCheckpointInterval = (long) (currentCheckpointInterval * 1.25);
+      return;
+    }
+    long interval = System.currentTimeMillis() - lastCheckpointTime;
+    if (interval >= currentCheckpointInterval) {
+      checkForBarrier();
+    }
+
+  }
 
   public void checkForBarrier() {
+    setResponsePending();
     sendBarrierSyncMessage();
   }
 
@@ -142,6 +153,18 @@ public abstract class SourceCheckpointableTask extends BaseStreamSource {
     return false;
   }
 
+  private synchronized void setResponsePending() {
+    this.responsePending = true;
+  }
+
+  private synchronized void resetResponsePending() {
+    this.responsePending = false;
+  }
+
+  private synchronized boolean getResponsePending() {
+    return this.responsePending;
+  }
+
   public class TaskClientConnectHandler implements ConnectHandler {
     @Override
     public void onError(SocketChannel channel) {
@@ -182,8 +205,11 @@ public abstract class SourceCheckpointableTask extends BaseStreamSource {
 
           emitBarrier();
           currentCheckpointInterval = globalCheckpointInterval;
-          intermediateMessageCount = 0;
+          lastCheckpointTime = System.currentTimeMillis();
+          resetResponsePending();
           currentBarrierID++;
+        } else {
+          resetResponsePending();
         }
       }
     }
