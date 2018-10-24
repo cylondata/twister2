7d751abd0a81560d073dcd1b6987070464114bc3
pulasthi <pulasthi911@gmail.com>
2018-10-24 14:45:50 -0400

    updating join

--- twister2/comms/src/java/edu/iu/dsc/tws/comms/dfw/io/join/JoinBatchFinalReceiver.java
+++ twister2/comms/src/java/edu/iu/dsc/tws/comms/dfw/io/join/JoinBatchFinalReceiver.java
@@ -11,20 +11,76 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.comms.dfw.io.join;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import edu.iu.dsc.tws.common.config.Config;
 import edu.iu.dsc.tws.comms.api.BulkReceiver;
 import edu.iu.dsc.tws.comms.api.DataFlowOperation;
+import edu.iu.dsc.tws.comms.api.MessageFlags;
 import edu.iu.dsc.tws.comms.api.MessageReceiver;
+import edu.iu.dsc.tws.comms.dfw.DataFlowPartition;
 
 public class JoinBatchFinalReceiver implements MessageReceiver {
+
+  private static final Logger LOG = Logger.getLogger(JoinBatchFinalReceiver.class.getName());
 
   /**
    * The receiver to be used to deliver the message
    */
   private BulkReceiver receiver;
+
+  /**
+   * The executor
+   */
+  protected int executor;
+
+  /**
+   * Keep the destination messages
+   */
+  private Map<Integer, List<Object>> targetMessagesLeft = new HashMap<>();
+  private Map<Integer, List<Object>> targetMessagesRight = new HashMap<>();
+  /**
+   * The dataflow operation left partition
+   */
+  private DataFlowOperation operationLeft;
+
+  /**
+   * The dataflow operation right partition
+   */
+  private DataFlowOperation operationRight;
+
+  /**
+   * The lock for excluding onMessage and communicationProgress
+   */
+  private Lock lock = new ReentrantLock();
+
+  /**
+   * These sources called onFinished
+   */
+  private Map<Integer, Set<Integer>> onFinishedSourcesLeft = new HashMap<>();
+  private Map<Integer, Set<Integer>> onFinishedSourcesRight = new HashMap<>();
+
+  /**
+   * The worker id
+   */
+  private int thisWorker;
+
+  /**
+   * sources for this operation
+   */
+  private Set<Integer> sources;
+
+  private Map<Integer, Boolean> targetDone = new HashMap<>();
 
   public JoinBatchFinalReceiver(BulkReceiver bulkReceiver) {
     this.receiver = bulkReceiver;
@@ -32,7 +88,25 @@ public class JoinBatchFinalReceiver implements MessageReceiver {
 
   @Override
   public void init(Config cfg, DataFlowOperation op, Map<Integer, List<Integer>> expectedIds) {
+    // The init method is called twice TODO: would be better to do a complete new data flow
+    // operation
+    if (operationLeft != null) {
+      this.operationRight = op;
+    } else {
+      executor = op.getTaskPlan().getThisExecutor();
+      thisWorker = op.getTaskPlan().getThisExecutor();
+      this.operationLeft = op;
+      this.sources = ((DataFlowPartition) op).getSources();
 
+      // lists to keep track of messages for destinations
+      for (int target : expectedIds.keySet()) {
+        targetDone.put(target, false);
+        targetMessagesLeft.put(target, new ArrayList<>());
+        targetMessagesRight.put(target, new ArrayList<>());
+        onFinishedSourcesLeft.put(target, new HashSet<>());
+        onFinishedSourcesRight.put(target, new HashSet<>());
+      }
+    }
   }
 
   @Override
@@ -55,11 +129,96 @@ public class JoinBatchFinalReceiver implements MessageReceiver {
    */
   @Override
   public boolean onMessage(int source, int path, int target, int flags, int tag, Object object) {
-    return false;
+
+    if (tag != 0 || tag != 1) {
+      throw new RuntimeException("Tag value must be either 0(left) or 1(right) for join operation");
+    }
+
+    lock.lock();
+    Map<Integer, List<Object>> targetMessages;
+    Map<Integer, Set<Integer>> onFinishedSources;
+
+    if (tag == 0) {
+      targetMessages = targetMessagesLeft;
+      onFinishedSources = onFinishedSourcesLeft;
+    } else {
+      targetMessages = targetMessagesRight;
+      onFinishedSources = onFinishedSourcesRight;
+    }
+
+    try {
+      Set<Integer> onFinishedSrcsTarget = onFinishedSources.get(target);
+      if ((flags & MessageFlags.END) == MessageFlags.END) {
+        if (onFinishedSrcsTarget.contains(source)) {
+          LOG.log(Level.WARNING,
+              String.format("%d Duplicate finish from source id %d", this.thisWorker, source));
+        } else {
+          onFinishedSrcsTarget.add(source);
+        }
+        return true;
+      }
+
+      List<Object> targetMsgList = targetMessages.get(target);
+      if (targetMsgList == null) {
+        throw new RuntimeException(String.format("%d target not exisits %d", executor, target));
+      }
+      if (object instanceof List) {
+        targetMsgList.addAll((Collection<?>) object);
+      } else {
+        targetMsgList.add(object);
+      }
+    } finally {
+      lock.unlock();
+    }
+    return true;
   }
 
   @Override
   public boolean progress() {
-    return false;
+    boolean needsFurtherProgress = false;
+    for (int target : targetDone.keySet()) {
+      if (targetDone.get(target)) {
+        continue;
+      }
+      lock.lock();
+      try {
+        if (checkIfFinished(target)) {
+          List<Object> results = innerJoin(targetMessagesLeft.get(target),
+              targetMessagesRight.get(target));
+        }
+      } finally {
+        lock.unlock();
+      }
+
+    }
+
+    return needsFurtherProgress;
+  }
+
+  /**
+   * Performs an inner join on the two data lists that have been provided.
+   * The join is performed using the key value.
+   *
+   * @param left left partition of the join
+   * @param right right partition of the join
+   * @return the joined list of values
+   */
+  private List<Object> innerJoin(List<Object> left, List<Object> right) {
+    List<Object> results = new ArrayList<>();
+    System.out.println(left.size());
+    System.out.println(right.size());
+    return results;
+  }
+
+  /**
+   * checks if all the messages for this target has been received for all partitions.
+   *
+   * @param target target to be checked
+   * @return true if all messages have been received and false otherwise
+   */
+  private boolean checkIfFinished(int target) {
+    return operationLeft.isDelegeteComplete() && operationRight.isDelegeteComplete()
+        && onFinishedSourcesLeft.get(target).equals(sources)
+        && onFinishedSourcesRight.get(target).equals(sources);
   }
 }
