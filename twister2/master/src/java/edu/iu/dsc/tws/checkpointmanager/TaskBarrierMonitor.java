5cc82828384362fdb517958b2e1bd0ab39f90789
kumar <inthira.14@cse.mrt.ac.lk>
2018-11-01 16:16:06 +0530

    add methods to store and restore acknowledged barrierID

--- twister2/master/src/java/edu/iu/dsc/tws/checkpointmanager/TaskBarrierMonitor.java
+++ twister2/master/src/java/edu/iu/dsc/tws/checkpointmanager/TaskBarrierMonitor.java
@@ -23,18 +23,27 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.checkpointmanager;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.protobuf.Message;
 
+import edu.iu.dsc.tws.checkpointmanager.state_backend.FsCheckpointStreamFactory;
+import edu.iu.dsc.tws.checkpointmanager.utils.CheckpointContext;
 import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.common.config.Context;
 import edu.iu.dsc.tws.common.net.tcp.request.MessageHandler;
 import edu.iu.dsc.tws.common.net.tcp.request.RRServer;
 import edu.iu.dsc.tws.common.net.tcp.request.RequestID;
+import edu.iu.dsc.tws.comms.utils.KryoSerializer;
+import edu.iu.dsc.tws.data.fs.Path;
+import edu.iu.dsc.tws.data.fs.local.LocalDataInputStream;
+import edu.iu.dsc.tws.data.fs.local.LocalFileSystem;
 import edu.iu.dsc.tws.proto.checkpoint.Checkpoint;
 
 public class TaskBarrierMonitor implements MessageHandler {
@@ -70,6 +79,8 @@ public class TaskBarrierMonitor implements MessageHandler {
     this.sinkParallelism = 0;
 
     this.allTaskGotRegistered = false;
+
+    System.out.println(config.getStringValue(Context.JOB_NAME));
   }
 
   @Override
@@ -112,8 +123,7 @@ public class TaskBarrierMonitor implements MessageHandler {
       LOG.fine("Source task " + taskId + " sent BarrierSync message.");
       Checkpoint.BarrierSync barrierSyncMessage = (Checkpoint.BarrierSync) message;
 
-      if (currentBarrierID == barrierSyncMessage.getCurrentBarrierID() && sendBarrierFlag
-          && allTaskGotRegistered) {
+      if (sendBarrierFlag && allTaskGotRegistered) {
 
         Checkpoint.BarrierSend barrierSendMessage = Checkpoint.BarrierSend.newBuilder()
             .setSendBarrier(true)
@@ -175,7 +185,14 @@ public class TaskBarrierMonitor implements MessageHandler {
 
       sendBarrierFlag = true;
       currentBarrierID = 1;
+      if (CheckpointContext.getCheckpointRecovery(config)) {
+        try {
+          currentBarrierID = readBarrierID() + 1;
+        } catch (Exception e) {
+          LOG.log(Level.WARNING, "could not read last acknowledge barrierID", e);
+        }
 
+      }
       currentBarrierReceivedSourceSet = new HashSet<Integer>();
 
       barrierCompleteReceivedSinkSet = new HashSet<Integer>();
@@ -194,6 +211,7 @@ public class TaskBarrierMonitor implements MessageHandler {
       currentBarrierReceivedSourceSet = new HashSet<Integer>();
       sendBarrierFlag = false;
 
+
       LOG.info("Barriers with Barrier ID " + currentBarrierID + " got sent from Source Tasks");
     }
   }
@@ -202,12 +220,59 @@ public class TaskBarrierMonitor implements MessageHandler {
     if (barrierCompleteReceivedSinkSet.size() == sinkParallelism) {
       barrierCompleteReceivedSinkSet = new HashSet<Integer>();
 
-      LOG.info("All sinks acknowledged checkpoint manager for barrier ID : " + currentBarrierID);
+      try {
+        writeBarrierID(currentBarrierID);
 
-      //start the next checkpoint
-      currentBarrierID++;
-      sendBarrierFlag = true;
+        LOG.info("All sinks acknowledged checkpoint manager for barrier ID : " + currentBarrierID);
+        //start the next checkpoint
+        currentBarrierID++;
+        sendBarrierFlag = true;
+      } catch (Exception e) {
+        LOG.log(Level.WARNING, "could not store acknowledge barrierID", e);
+      }
 
     }
   }
+
+  private void writeBarrierID(int barrierID) throws Exception {
+    synchronized (this) {
+      Path path = new Path(new File(CheckpointContext
+          .getStatebackendDirectoryDefault(config)).toURI());
+//      Path path = new Path("/home/kumar/statebackend/");
+      Path path2 = new Path(path, config.getStringValue(Context.JOB_NAME));
+      LocalFileSystem localFileSystem = new LocalFileSystem();
+      FsCheckpointStreamFactory fs = new FsCheckpointStreamFactory(path2, path2,
+          0, localFileSystem);
+      KryoSerializer kryoSerializer = new KryoSerializer();
+      byte[] checkpoint = kryoSerializer.serialize(barrierID);
+      FsCheckpointStreamFactory.FsCheckpointStateOutputStream stream =
+          fs.createCheckpointStateOutputStream();
+      stream.initialize("Acknowledged", "BarrierIDs");
+      stream.write(checkpoint);
+      stream.closeWriting();
+    }
+  }
+
+  private int readBarrierID() throws Exception {
+    Path path = new Path(new File(CheckpointContext
+        .getStatebackendDirectoryDefault(config)).toURI());
+    Path path2 = new Path(path, config.getStringValue(Context.JOB_NAME));
+    LocalFileSystem localFileSystem = new LocalFileSystem();
+    FsCheckpointStreamFactory fs = new FsCheckpointStreamFactory(path2, path2,
+        0, localFileSystem);
+    FsCheckpointStreamFactory.FsCheckpointStateOutputStream stream =
+        fs.createCheckpointStateOutputStream();
+    LocalDataInputStream localDataReadStream = (LocalDataInputStream)
+        stream.openStateHandle("Acknowledged",
+            "BarrierIDs").openInputStream();
+    byte[] checkpoint;
+    synchronized (this) {
+      checkpoint = stream.readCheckpoint(localDataReadStream);
+    }
+    KryoSerializer kryoSerializer = new KryoSerializer();
+    return (int) kryoSerializer.deserialize(checkpoint);
+
+  }
+
+
 }
