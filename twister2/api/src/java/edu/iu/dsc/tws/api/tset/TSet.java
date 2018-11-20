445b598c12778596d62876b25e6d724c426f0ba9
supunkamburugamuva <supun06@gmail.com>
2018-11-20 16:05:50 -0500

    intial TSet API

---
+++ twister2/api/src/java/edu/iu/dsc/tws/api/tset/TSet.java
@@ -0,0 +1,43 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.api.tset;
+
+/**
+ * Twister data set.
+ *
+ * @param <T> type of the data set
+ */
+public interface TSet<T> {
+
+  /**
+   *
+   * @param mapFn
+   * @param <P>
+   * @return
+   */
+  <P> TSet<P> map(MapFunction<T, ? extends P> mapFn);
+
+  /**
+   *
+   * @param mapFn
+   * @param <P>
+   * @return
+   */
+  <P> TSet<P> flatMap(FlatMapFunction<T, ? extends P> mapFn);
+
+  /**
+   *
+   * @param reduceFn
+   * @return
+   */
+  TSet<T> reduce(ReduceFunction<T> reduceFn);
+}
