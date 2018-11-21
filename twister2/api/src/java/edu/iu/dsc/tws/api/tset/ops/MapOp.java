c84382b8552567171291da5b6ca46c1d8ef79e72
supunkamburugamuva <supun06@gmail.com>
2018-11-21 12:46:21 -0500

    initial TSet API

---
+++ twister2/api/src/java/edu/iu/dsc/tws/api/tset/ops/MapOp.java
@@ -0,0 +1,41 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.api.tset.ops;
+
+import edu.iu.dsc.tws.api.tset.MapFunction;
+import edu.iu.dsc.tws.common.config.Config;
+import edu.iu.dsc.tws.task.api.ICompute;
+import edu.iu.dsc.tws.task.api.IMessage;
+import edu.iu.dsc.tws.task.api.TaskContext;
+
+public class MapOp<T, R> implements ICompute {
+  private MapFunction<T, R> mapFn;
+
+  private TaskContext context;
+
+  public MapOp(MapFunction<T, R> mapFn) {
+    this.mapFn = mapFn;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public boolean execute(IMessage content) {
+    T data = (T) content.getContent();
+    R result = mapFn.map(data);
+    return context.write("DEFAULT", result);
+  }
+
+  @Override
+  public void prepare(Config cfg, TaskContext ctx) {
+    this.context = ctx;
+  }
+}
