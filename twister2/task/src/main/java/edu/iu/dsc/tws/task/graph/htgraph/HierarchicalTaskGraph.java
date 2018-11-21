362186ef1e4560f7942b6376f70820c952a476eb
kannang83 <kannan.gridlab@gmail.com>
2018-11-21 11:42:56 -0500

    HTG UPDATE

--- twister2/task/src/main/java/edu/iu/dsc/tws/task/graph/htgraph/HierarchicalTaskGraph.java
+++ twister2/task/src/main/java/edu/iu/dsc/tws/task/graph/htgraph/HierarchicalTaskGraph.java
@@ -9,7 +9,19 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-package edu.iu.dsc.tws.task.graph;
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.task.graph.htgraph;
 
 import java.util.Comparator;
 import java.util.HashMap;
@@ -17,6 +29,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
+
+import edu.iu.dsc.tws.task.graph.DataFlowTaskGraph;
+import edu.iu.dsc.tws.task.graph.Edge;
+import edu.iu.dsc.tws.task.graph.OperationMode;
 
 public class HierarchicalTaskGraph extends BaseHierarchicalTaskGraph<DataFlowTaskGraph, Edge> {
 
