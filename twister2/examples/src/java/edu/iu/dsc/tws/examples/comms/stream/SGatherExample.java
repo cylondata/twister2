21449bad507dc95347f2776b005944f8691a1aec
Chathura Widanage <chathurawidanage@gmail.com>
2019-02-27 13:15:21 -0500

    Removed unused import from SGather

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/comms/stream/SGatherExample.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/comms/stream/SGatherExample.java
@@ -16,7 +16,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.iu.dsc.tws.common.config.Config;
