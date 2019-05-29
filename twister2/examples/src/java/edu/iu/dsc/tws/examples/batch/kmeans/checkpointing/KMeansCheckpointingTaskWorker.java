2a6d7f2b788c227901f41ca6c12728ad08398353
niranda perera <niranda.perera@gmail.com>
2019-05-29 10:35:56 -0400

    adding kmeans checkpointing taskworker example

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/batch/kmeans/checkpointing/KMeansCheckpointingTaskWorker.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/batch/kmeans/checkpointing/KMeansCheckpointingTaskWorker.java
@@ -10,43 +10,23 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
 package edu.iu.dsc.tws.examples.batch.kmeans.checkpointing;
 
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import edu.iu.dsc.tws.api.dataobjects.DataFileReplicatedReadSource;
-import edu.iu.dsc.tws.api.dataobjects.DataObjectSource;
-import edu.iu.dsc.tws.api.task.ComputeConnection;
-import edu.iu.dsc.tws.api.task.TaskGraphBuilder;
 import edu.iu.dsc.tws.api.task.ftolerance.CheckpointingTaskWorker;
-import edu.iu.dsc.tws.common.config.Context;
 import edu.iu.dsc.tws.comms.dfw.io.types.ObjectPacker;
 import edu.iu.dsc.tws.comms.dfw.io.types.primitive.IntegerPacker;
-import edu.iu.dsc.tws.data.api.DataType;
 import edu.iu.dsc.tws.dataset.DataObject;
 import edu.iu.dsc.tws.dataset.DataPartition;
-import edu.iu.dsc.tws.examples.batch.kmeans.KMeansDataObjectCompute;
-import edu.iu.dsc.tws.examples.batch.kmeans.KMeansDataObjectDirectSink;
 import edu.iu.dsc.tws.examples.batch.kmeans.KMeansWorker;
 import edu.iu.dsc.tws.examples.batch.kmeans.KMeansWorkerParameters;
 import edu.iu.dsc.tws.examples.batch.kmeans.KMeansWorkerUtils;
 import edu.iu.dsc.tws.executor.api.ExecutionPlan;
 import edu.iu.dsc.tws.ftolerance.api.Snapshot;
 import edu.iu.dsc.tws.task.graph.DataFlowTaskGraph;
-import edu.iu.dsc.tws.task.graph.OperationMode;
 
 /**
  * It is the main class for the K-Means clustering which consists of four main tasks namely
@@ -55,6 +35,15 @@ import edu.iu.dsc.tws.task.graph.OperationMode;
  */
 public class KMeansCheckpointingTaskWorker extends CheckpointingTaskWorker {
   private static final Logger LOG = Logger.getLogger(KMeansCheckpointingTaskWorker.class.getName());
+
+  private static final String I_KEY = "iter";
+  private static final String CENT_OBJ = "centDataObj";
+
+  @Override
+  public void prepare(Snapshot checkpoint) {
+    checkpoint.setPacker(I_KEY, IntegerPacker.getInstance());
+    checkpoint.setPacker(CENT_OBJ, ObjectPacker.getInstance());
+  }
 
   /**
    * First, the execute method invokes the generateDataPoints method to generate the datapoints file
@@ -68,7 +57,7 @@ public class KMeansCheckpointingTaskWorker extends CheckpointingTaskWorker {
    */
   @SuppressWarnings("unchecked")
   @Override
-  public void execute(Snapshot snapshot) {
+  public void execute(Snapshot checkpoint) {
     LOG.log(Level.INFO, "Task worker starting: " + workerId);
 
     KMeansWorkerParameters kMeansJobParameters = KMeansWorkerParameters.build(config);
@@ -100,17 +89,21 @@ public class KMeansCheckpointingTaskWorker extends CheckpointingTaskWorker {
     DataObject<Object> dataPointsObject = taskExecutor.getOutput(
         datapointsTaskGraph, firstGraphExecutionPlan, "datapointsink");
 
-
-    /* Second Graph to read the centroids **/
-    DataFlowTaskGraph centroidsTaskGraph = KMeansWorker.buildCentroidsTG(centroidDirectory, csize,
-        parallelismValue, dimension, config);
-    //Get the execution plan for the second task graph
-    ExecutionPlan secondGraphExecutionPlan = taskExecutor.plan(centroidsTaskGraph);
-    //Actual execution for the second taskgraph
-    taskExecutor.execute(centroidsTaskGraph, secondGraphExecutionPlan);
-    //Retrieve the output of the first task graph
-    DataObject<Object> centroidsDataObject = taskExecutor.getOutput(
-        centroidsTaskGraph, secondGraphExecutionPlan, "centroidsink");
+    DataObject<Object> centroidsDataObject;
+    if (!checkpoint.isCheckpointed(CENT_OBJ)) {
+      /* Second Graph to read the centroids **/
+      DataFlowTaskGraph centroidsTaskGraph = KMeansWorker.buildCentroidsTG(centroidDirectory, csize,
+          parallelismValue, dimension, config);
+      //Get the execution plan for the second task graph
+      ExecutionPlan secondGraphExecutionPlan = taskExecutor.plan(centroidsTaskGraph);
+      //Actual execution for the second taskgraph
+      taskExecutor.execute(centroidsTaskGraph, secondGraphExecutionPlan);
+      //Retrieve the output of the first task graph
+      centroidsDataObject = taskExecutor.getOutput(
+          centroidsTaskGraph, secondGraphExecutionPlan, "centroidsink");
+    } else {
+      centroidsDataObject = (DataObject<Object>) checkpoint.get(CENT_OBJ);
+    }
 
     long endTimeData = System.currentTimeMillis();
 
@@ -120,7 +113,9 @@ public class KMeansCheckpointingTaskWorker extends CheckpointingTaskWorker {
 
     //Perform the iterations from 0 to 'n' number of iterations
     ExecutionPlan plan = taskExecutor.plan(kmeansTaskGraph);
-    for (int i = 0; i < iterations; i++) {
+
+    int i = (int) checkpoint.getOrDefault(I_KEY, 0);      // recover from checkpoint
+    for (; i < iterations; i++) {
       //add the datapoints and centroids as input to the kmeanssource task.
       taskExecutor.addInput(
           kmeansTaskGraph, plan, "kmeanssource", "points", dataPointsObject);
@@ -130,6 +125,9 @@ public class KMeansCheckpointingTaskWorker extends CheckpointingTaskWorker {
       taskExecutor.itrExecute(kmeansTaskGraph, plan);
       //retrieve the new centroid value for the next iterations
       centroidsDataObject = taskExecutor.getOutput(kmeansTaskGraph, plan, "kmeanssink");
+
+      // at each iteration, commit the checkpoint.
+      commitCheckpoint();
     }
     taskExecutor.waitFor(kmeansTaskGraph, plan);
 
@@ -145,11 +143,6 @@ public class KMeansCheckpointingTaskWorker extends CheckpointingTaskWorker {
         + Arrays.deepToString(centroid));
   }
 
-  @Override
-  public void prepare(Snapshot snapshot) {
-    snapshot.setPacker("iter", IntegerPacker.getInstance());
-    snapshot.setPacker("centroidsDataObj", ObjectPacker.getInstance());
-  }
 
 }
 
