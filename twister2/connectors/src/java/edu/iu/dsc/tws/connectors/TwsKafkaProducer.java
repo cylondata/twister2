1ec60bd4f29c413b65c3e47b56ae33f10cbab71d
Chathura Widanage <chathurawidanage@gmail.com>
2019-06-17 15:41:49 -0400

    removed unnecessary classes

--- twister2/connectors/src/java/edu/iu/dsc/tws/connectors/TwsKafkaProducer.java
+++ twister2/connectors/src/java/edu/iu/dsc/tws/connectors/TwsKafkaProducer.java
@@ -25,11 +25,12 @@ import org.slf4j.LoggerFactory;
 import edu.iu.dsc.tws.common.config.Config;
 import edu.iu.dsc.tws.connectors.config.KafkaConsumerConfig;
 import edu.iu.dsc.tws.connectors.config.KafkaProducerConfig;
+import edu.iu.dsc.tws.task.api.BaseSink;
 import edu.iu.dsc.tws.task.api.IMessage;
 import edu.iu.dsc.tws.task.api.TaskContext;
-import edu.iu.dsc.tws.task.api.checkpoint.SinkCheckpointableTask;
 
-public class TwsKafkaProducer<T> extends SinkCheckpointableTask {
+public class TwsKafkaProducer<T> extends BaseSink {
+
   private static final long serialVersionUID = -264264120110286749L;
   private static Logger log = LoggerFactory.getLogger(TwsKafkaProducer.class);
   private Properties kafkaConfigs;
