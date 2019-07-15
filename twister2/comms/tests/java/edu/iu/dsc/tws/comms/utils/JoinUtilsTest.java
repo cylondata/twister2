a1052ecc81c5a4fff9db7b0d1cd9c8515020de6f
Chathura Widanage <chathurawidanage@gmail.com>
2019-07-15 09:34:28 -0400

    Adding wikipedia example as a test case

--- twister2/comms/tests/java/edu/iu/dsc/tws/comms/utils/JoinUtilsTest.java
+++ twister2/comms/tests/java/edu/iu/dsc/tws/comms/utils/JoinUtilsTest.java
@@ -34,12 +34,76 @@ public class JoinUtilsTest {
 
   private static final Logger LOG = Logger.getLogger(JoinUtilsTest.class.getName());
 
+  /**
+   * Example values from https://en.wikipedia.org/wiki/Join_(SQL)
+   */
+  private List<Tuple> getEmployees() {
+    List<Tuple> employees = new ArrayList<>();
+
+    employees.add(Tuple.of(31, "Rafferty"));
+    employees.add(Tuple.of(33, "Jones"));
+    employees.add(Tuple.of(33, "Heisenberg"));
+    employees.add(Tuple.of(34, "Robinson"));
+    employees.add(Tuple.of(34, "Smith"));
+    employees.add(Tuple.of(null, "Williams"));
+
+    return employees;
+  }
+
+  private List<Tuple> getDepartments() {
+    List<Tuple> departments = new ArrayList<>();
+
+    departments.add(Tuple.of(31, "Sales"));
+    departments.add(Tuple.of(33, "Engineering"));
+    departments.add(Tuple.of(34, "Clerical"));
+    departments.add(Tuple.of(35, "Marketing"));
+
+    return departments;
+  }
+
+  private List<Object> getInnerJoined() {
+    List<Object> innerJoined = new ArrayList<>();
+    innerJoined.add(new JoinedTuple(34, "Robinson", "Clerical"));
+    innerJoined.add(new JoinedTuple(33, "Jones", "Engineering"));
+    innerJoined.add(new JoinedTuple(34, "Smith", "Clerical"));
+    innerJoined.add(new JoinedTuple(33, "Heisenberg", "Engineering"));
+    innerJoined.add(new JoinedTuple(31, "Rafferty", "Sales"));
+    return innerJoined;
+  }
+
+  private KeyComparatorWrapper getEmployeeDepComparator() {
+    return new KeyComparatorWrapper((Comparator<Integer>) (o1, o2) -> {
+      if (o1 == null) {
+        return 1;
+      } else if (o2 == null) {
+        return -1;
+      }
+      return o1.compareTo(o2);
+    });
+  }
+
+  private Comparator<Object> getJoinedTupleComparator() {
+    return new Comparator<Object>() {
+      @Override
+      public int compare(Object o1, Object o2) {
+        Integer k1 = (Integer) ((JoinedTuple) o1).getKey();
+        Integer k2 = (Integer) ((JoinedTuple) o2).getKey();
+        if (k1 == null) {
+          return 1;
+        } else if (k2 == null) {
+          return -1;
+        }
+        return k1.compareTo(k2);
+      }
+    };
+  }
+
   @Test
   public void innerJoin() {
     List<Tuple> left = new ArrayList<>();
     List<Tuple> right = new ArrayList<>();
     Random random = new Random();
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 100; i++) {
       left.add(Tuple.of(random.nextInt(10), random.nextInt()));
       right.add(Tuple.of(random.nextInt(10), random.nextInt()));
     }
@@ -123,5 +187,28 @@ public class JoinUtilsTest {
     Assert.assertEquals(i, objects.size());
 
     LOG.info(objects.toString());
+  }
+
+  @Test
+  public void innerJoinReal() {
+    List<Tuple> employees = this.getEmployees();
+    List<Tuple> departments = this.getDepartments();
+    List<Object> joined = JoinUtils.innerJoin(
+        employees,
+        departments,
+        this.getEmployeeDepComparator()
+    );
+
+    List<Object> innerJoined = this.getInnerJoined();
+
+    joined.sort(this.getJoinedTupleComparator());
+    innerJoined.sort(this.getJoinedTupleComparator());
+
+    Assert.assertEquals(joined.size(), innerJoined.size());
+
+    for (int i = 0; i < innerJoined.size(); i++) {
+      Assert.assertEquals(((JoinedTuple) innerJoined.get(i)).getKey(),
+          ((JoinedTuple) joined.get(i)).getKey());
+    }
   }
 }
