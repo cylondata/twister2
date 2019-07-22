deb650dbc3f4aa2dcdbc86e61745f0964f953cf9
supunkamburugamuva <supun06@gmail.com>
2019-07-22 11:41:30 -0400

    adding domain name

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -25,7 +25,7 @@ const baseUrl = '/';
 const siteConfig = {
     title: 'Twister2', // Title for your website.
     tagline: 'High Performance Data Analytics',
-    url: 'https://dsc-spidal.github.io', // Your website URL
+    url: 'http://twister2.org', // Your website URL
     baseUrl: '/twister2/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -42,7 +42,7 @@ const siteConfig = {
     headerLinks: [
         {doc: 'introduction', label: 'Docs'},
         {page: 'docs/download', label: 'Download'},
-        {href: "https://github.com/dsc-spidal/twister2", label: "GitHub"},
+        {href: "http://twister2.org", label: "GitHub"},
         {blog: true, label: 'Blog'},
     ],
 
