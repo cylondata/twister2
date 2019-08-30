5fd21d1afac789d7948ab818f73fb8098d338129
kannang83 <kannan.gridlab@gmail.com>
2019-08-30 14:39:26 -0400

    updates to the connected dataflow

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/batch/cdfw/CDFConstants.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/batch/cdfw/CDFConstants.java
@@ -15,6 +15,7 @@ public class CDFConstants {
 
   public static final String ARGS_WORKERS = "workers";
   public static final String ARGS_PARALLELISM_VALUE = "parallelism";
+  public static final String ARGS_DIMENSIONS = "dim";
   public static final String ARGS_DUMMY = "dummy"; // todo: remove this
   public static final String ARGS_NUMBER_OF_ROWS = "rows";
   public static final String ARGS_NUMBER_OF_DIMENSIONS = "dim";
