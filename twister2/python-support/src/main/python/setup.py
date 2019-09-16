74080e4e2821a911dffc8a6d56619adb07133585
Chathura Widanage <chathurawidanage@gmail.com>
2019-09-16 10:48:13 -0400

    Changes to pip script

--- twister2/python-support/src/main/python/setup.py
+++ twister2/python-support/src/main/python/setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("../../../../../README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='Twister2',
-    version='0.1',
+    name='twister2',
+    version='0.1.1',
     author="Twister2 Developers",
     author_email="twister2@googlegroups.com",
     description="Twister2 is a composable big data environment supporting streaming, data pipelines and analytics. Our vision is to build robust, simple to use data analytics solutions that can leverage both clouds and high performance computing infrastructure.",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
--- twister2/python-support/src/main/python/setup_pip.sh
+++ twister2/python-support/src/main/python/setup_pip.sh
@@ -1,2 +1,8 @@
+# Creating distribution
 python3 setup.py bdist_wheel
-python3 -m pip install dist/twister2-0.1-py3-none-any.whl
+
+# Installing Locally
+python3 -m pip install dist/twister2-0.1.1-py3-none-any.whl
+
+# Uploading to PIP
+python3 -m twine upload dist/*
