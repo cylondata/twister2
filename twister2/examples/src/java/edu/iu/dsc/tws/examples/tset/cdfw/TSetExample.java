e026c83c17b4dfd6dd93f6d2b244fda8ea5833eb
pulasthi <pulasthi911@gmail.com>
2019-09-20 01:10:44 -0400

    adding tset cdfw example

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/tset/cdfw/TSetExample.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/tset/cdfw/TSetExample.java
@@ -11,39 +11,110 @@
 //  limitations under the License.
 package edu.iu.dsc.tws.examples.tset.cdfw;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
-import edu.iu.dsc.tws.api.tset.cdfw.BatchTSetBaseDriver;
-import edu.iu.dsc.tws.api.tset.cdfw.BatchTSetCDFWEnvironment;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import edu.iu.dsc.tws.api.JobConfig;
+import edu.iu.dsc.tws.api.Twister2Job;
+import edu.iu.dsc.tws.api.config.Config;
+import edu.iu.dsc.tws.api.scheduler.SchedulerContext;
+import edu.iu.dsc.tws.api.tset.fn.ApplyFunc;
 import edu.iu.dsc.tws.api.tset.fn.SourceFunc;
+import edu.iu.dsc.tws.examples.batch.cdfw.CDFConstants;
+import edu.iu.dsc.tws.rsched.core.ResourceAllocator;
+import edu.iu.dsc.tws.rsched.job.Twister2Submitter;
+import edu.iu.dsc.tws.task.impl.cdfw.CDFWWorker;
+import edu.iu.dsc.tws.tset.cdfw.BatchTSetBaseDriver;
+import edu.iu.dsc.tws.tset.cdfw.BatchTSetCDFWEnvironment;
 import edu.iu.dsc.tws.tset.links.batch.DirectTLink;
 import edu.iu.dsc.tws.tset.sets.batch.SourceTSet;
 
-public class TSetExample {
+public final class TSetExample {
   private static final Logger LOG = Logger.getLogger(TSetExample.class.getName());
 
-  public static class Driver extends BatchTSetBaseDriver{
+  private TSetExample() {
+  }
+
+  public static class Driver extends BatchTSetBaseDriver {
 
     @Override
     public void execute(BatchTSetCDFWEnvironment env) {
-      SourceTSet<Integer> src = env.createSource(new SourceFunc<Integer>() {
-        private int c = 0;
-
-        @Override
-        public boolean hasNext() {
-          return c < 10;
-        }
-
-        @Override
-        public Integer next() {
-          return c++;
-        }
-      }, 2);
+      SourceTSet<Integer> src = env.createSource(new FirstSourceFunc(), 2);
 
       DirectTLink<Integer> direct = src.direct().setName("direct");
 
       LOG.info("test foreach");
-      direct.forEach(i -> LOG.info("foreach: " + i));
+      direct.forEach(new ForeachFunc());
     }
+  }
+
+  private static class ForeachFunc implements ApplyFunc<Integer> {
+
+    @Override
+    public void apply(Integer data) {
+      System.out.println(data);
+    }
+  }
+
+  private static class FirstSourceFunc implements SourceFunc<Integer> {
+    private int c = 0;
+
+    @Override
+    public boolean hasNext() {
+      return c < 10;
+    }
+
+    @Override
+    public Integer next() {
+      return c++;
+    }
+  }
+
+  public static void main(String[] args) throws ParseException {
+    // first load the configurations from command line and config files
+    Config config = ResourceAllocator.loadConfig(new HashMap<>());
+
+    // build JobConfig
+    HashMap<String, Object> configurations = new HashMap<>();
+    configurations.put(SchedulerContext.THREADS_PER_WORKER, 1);
+
+    Options options = new Options();
+    options.addOption(CDFConstants.ARGS_PARALLELISM_VALUE, true, "2");
+    options.addOption(CDFConstants.ARGS_WORKERS, true, "2");
+
+    @SuppressWarnings("deprecation")
+    CommandLineParser commandLineParser = new DefaultParser();
+    CommandLine commandLine = commandLineParser.parse(options, args);
+
+    int instances = Integer.parseInt(commandLine.getOptionValue(CDFConstants.ARGS_WORKERS));
+    int parallelismValue =
+        Integer.parseInt(commandLine.getOptionValue(CDFConstants.ARGS_PARALLELISM_VALUE));
+
+    configurations.put(CDFConstants.ARGS_WORKERS, Integer.toString(instances));
+    configurations.put(CDFConstants.ARGS_PARALLELISM_VALUE, Integer.toString(parallelismValue));
+
+    // build JobConfig
+    JobConfig jobConfig = new JobConfig();
+    jobConfig.putAll(configurations);
+
+    config = Config.newBuilder().putAll(config)
+        .put(SchedulerContext.DRIVER_CLASS, null).build();
+
+    Twister2Job twister2Job;
+    twister2Job = Twister2Job.newBuilder()
+        .setWorkerClass(CDFWWorker.class)
+        .setJobName(TSetExample.class.getName())
+        .setDriverClass(TSetExample.Driver.class.getName())
+        .addComputeResource(1, 512, instances)
+        .setConfig(jobConfig)
+        .build();
+    // now submit the job
+    Twister2Submitter.submitJob(twister2Job, config);
   }
 }
