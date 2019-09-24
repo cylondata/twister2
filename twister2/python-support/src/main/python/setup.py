c82f127b417eb7f691b8e4be46eb98dfe272f83e
Chathura Widanage <chathurawidanage@gmail.com>
2019-09-24 16:49:26 -0400

    Updating python requirement

--- twister2/python-support/src/main/python/setup.py
+++ twister2/python-support/src/main/python/setup.py
@@ -8,7 +8,7 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name='twister2',
-    version='0.1.2',
+    version='0.1.4',
     author="Twister2 Developers",
     author_email="twister2@googlegroups.com",
     description="Twister2 is a composable big data environment supporting streaming, data pipelines and analytics. Our vision is to build robust, simple to use data analytics solutions that can leverage both clouds and high performance computing infrastructure.",
@@ -21,10 +21,11 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'jep',
         'cloudpickle',
-        'numpy'
+        'numpy',
+        'py4j'
     ],
 )
