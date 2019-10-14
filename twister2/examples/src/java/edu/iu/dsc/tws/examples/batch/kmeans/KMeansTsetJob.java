3d8eba47d13ab99162eba9ce91e31e977d40fbb7
nirandaperera <niranda.perera@gmail.com>
2019-10-13 22:39:03 -0400

    fixing api for kmeans and svm

--- twister2/examples/src/java/edu/iu/dsc/tws/examples/batch/kmeans/KMeansTsetJob.java
+++ twister2/examples/src/java/edu/iu/dsc/tws/examples/batch/kmeans/KMeansTsetJob.java
@@ -65,9 +65,9 @@ public class KMeansTsetJob implements BatchTSetIWorker, Serializable {
 
     long startTime = System.currentTimeMillis();
     CachedTSet<double[][]> points =
-        tc.createSource(new PointsSource(), parallelismValue).setName("dataSource").cache(false);
+        tc.createSource(new PointsSource(), parallelismValue).setName("dataSource").cache();
     CachedTSet<double[][]> centers =
-        tc.createSource(new CenterSource(), parallelismValue).cache(false);
+        tc.createSource(new CenterSource(), parallelismValue).cache();
 
     long endTimeData = System.currentTimeMillis();
     ComputeTSet<double[][], Iterator<double[][]>> kmeansTSet =
@@ -86,12 +86,14 @@ public class KMeansTsetJob implements BatchTSetIWorker, Serializable {
           return newCentroids;
         }).map(new AverageCenters());
 
+    kmeansTSet.addInput("centers", centers);
+    centers = reduced.lazyCache();
+
     for (int i = 0; i < iterations; i++) {
-      kmeansTSet.addInput("centers", centers);
-      centers = reduced.cache(true);
+      tc.eval(centers);
     }
 
-    reduced.finishIter();
+    tc.finishEval(centers);
 
     DataPartition<?> centroidPartition = centers.getDataObject().getPartition(workerId);
     double[][] centroid = null;
@@ -126,8 +128,8 @@ public class KMeansTsetJob implements BatchTSetIWorker, Serializable {
 
     @Override
     public double[][] map(double[][] data) {
-      double[][] centers = (double[][]) getTSetContext().getInput("centers").
-          getPartition(getTSetContext().getIndex()).getConsumer().next();
+      double[][] centers = (double[][]) getTSetContext()
+          .getInput("centers").getConsumer().next();
       KMeansCalculator kMeansCalculator = new KMeansCalculator(data, centers, dimension);
       return kMeansCalculator.calculate();
     }
