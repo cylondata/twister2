54ce77d5889209bdb655abf3b32deef6ef050ec9
Chathura Widanage <chathurawidanage@gmail.com>
2019-10-16 12:18:13 -0400

    Adding HDFS backed collection partition

---
+++ twister2/data/src/main/java/edu/iu/dsc/tws/dataset/partition/HDFSBackedCollectionPartition.java
@@ -0,0 +1,67 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.dataset.partition;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.UUID;
+
+import edu.iu.dsc.tws.api.comms.messaging.types.MessageType;
+import edu.iu.dsc.tws.api.config.Config;
+import edu.iu.dsc.tws.api.data.FileSystem;
+import edu.iu.dsc.tws.api.data.Path;
+import edu.iu.dsc.tws.api.exceptions.Twister2RuntimeException;
+import edu.iu.dsc.tws.data.utils.FileSystemUtils;
+
+public class HDFSBackedCollectionPartition<T> extends BufferedCollectionPartition<T> {
+
+  private static final String CONFIG_HDFS_ROOT = "twister2.data.hdfs.root";
+  private static final String HDFS_PROTO = "hdfs://";
+
+  public HDFSBackedCollectionPartition(int maxFramesInMemory, MessageType dataType,
+                                       int bufferedBytes, Config config) {
+    super(maxFramesInMemory, dataType, bufferedBytes, config);
+  }
+
+  public HDFSBackedCollectionPartition(int maxFramesInMemory, Config config) {
+    super(maxFramesInMemory, config);
+  }
+
+  public HDFSBackedCollectionPartition(int maxFramesInMemory, MessageType dataType, Config config) {
+    super(maxFramesInMemory, dataType, config);
+  }
+
+  public HDFSBackedCollectionPartition(MessageType dataType, int bufferedBytes, Config config) {
+    super(dataType, bufferedBytes, config);
+  }
+
+  protected String getRootPathStr(Config config) {
+    return HDFS_PROTO + String.join(File.separator,
+        config.getStringValue(CONFIG_HDFS_ROOT), String.join(File.separator,
+            UUID.randomUUID().toString()));
+  }
+
+  @Override
+  protected FileSystem getFileSystem(Config config) {
+    try {
+      return FileSystemUtils.get(URI.create(getRootPathStr(config)), config);
+    } catch (IOException e) {
+      throw new Twister2RuntimeException("Error in connecting to file system", e);
+    }
+  }
+
+  @Override
+  protected Path getRootPath(Config config) {
+    return new Path(getRootPathStr(config));
+  }
+}
---
+++ twister2/data/src/test/java/edu/iu/dsc/tws/data/HDFSBackedCollectionPartitionTest.java
@@ -0,0 +1,64 @@
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package edu.iu.dsc.tws.data;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.iu.dsc.tws.api.comms.messaging.types.MessageTypes;
+import edu.iu.dsc.tws.api.dataset.DataPartitionConsumer;
+import edu.iu.dsc.tws.common.config.ConfigLoader;
+import edu.iu.dsc.tws.dataset.partition.BufferedCollectionPartition;
+import edu.iu.dsc.tws.dataset.partition.HDFSBackedCollectionPartition;
+
+public class HDFSBackedCollectionPartitionTest {
+
+  @Test
+  public void testIO() {
+
+    try (BufferedCollectionPartition<Integer> dbp = new HDFSBackedCollectionPartition<>(
+        10, MessageTypes.INTEGER, 1000,
+        ConfigLoader.loadTestConfig()
+    )) {
+
+      List<Integer> rawData = new ArrayList<>();
+
+      for (int i = 0; i < 1000000 / Integer.BYTES; i++) {
+        rawData.add(i);
+        dbp.add(i);
+      }
+
+      DataPartitionConsumer<Integer> consumer = dbp.getConsumer();
+      Iterator<Integer> rawIterator = rawData.iterator();
+      while (consumer.hasNext()) {
+        Assert.assertEquals(consumer.next(), rawIterator.next());
+      }
+      dbp.clear();
+    }
+  }
+}
