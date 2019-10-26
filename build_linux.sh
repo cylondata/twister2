1642a4515af6a5f6d342ea498410c85403039c52
Chathura Widanage <chathurawidanage@gmail.com>
2019-10-26 11:55:46 -0400

    Adding system dependencies to a separate script

--- build_linux.sh
+++ build_linux.sh
@@ -11,10 +11,12 @@ else
   echo "Found java at $JAVA_HOME"
 fi
 
+# install system dependencies
+sh install_sys_dep.sh
+
 echo "Looking for bazel..."
 if ! exists bazel; then
   echo "Bazel not found on this system. Installing bazel..." >&2
-  sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python3
   wget https://github.com/bazelbuild/bazel/releases/download/1.1.0/bazel-1.1.0-installer-linux-x86_64.sh -O /tmp/bazel.sh
   chmod 700 /tmp/bazel.sh
   /tmp/bazel.sh --user
@@ -22,12 +24,6 @@ if ! exists bazel; then
 else
   echo "Found bazel at $(command -v bazel)"
 fi
-
-echo "Installing required tools..."
-sudo apt-get update
-sudo apt-get install g++ git build-essential automake cmake libtool-bin zip libunwind-setjmp0-dev zlib1g-dev unzip pkg-config python-setuptools -y
-sudo apt-get install python3-dev python3-pip
-sudo pip3 install wheel
 
 echo "Building twister2..."
 bazel build --config=ubuntu scripts/package:tarpkgs
