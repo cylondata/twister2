74aa9d4379228d94703006bbcf219db1b05207e5
Chathura Widanage <chathurawidanage@gmail.com>
2019-12-02 16:11:23 -0500

    Adding keyed gather with all options

--- twister2/python-support/src/main/python/twister2/tset/KeyedTSet.py
+++ twister2/python-support/src/main/python/twister2/tset/KeyedTSet.py
@@ -1,4 +1,5 @@
 import twister2.tset.TLink as Tl
+from twister2.tset.fn import PartitionFunc
 from twister2.utils import function_wrapper
 
 
@@ -10,13 +11,53 @@ class KeyedTSet:
     def direct(self):
         return Tl.TLink(self.__java_ref.keyedDirect(), self.__env)
 
-    def reduce(self, reduce_func):
+    def keyed_reduce(self, reduce_func):
         reduce_wrapper = function_wrapper(reduce_func)
         reduce_func_java_ref = self.__env.functions.reduce.build(reduce_wrapper)
         reduce_t_link_java_ref = self.__java_ref.keyedReduce(reduce_func_java_ref)
         return Tl.TLink(reduce_t_link_java_ref, self.__env)
 
+    def keyed_gather(self, partition_func: PartitionFunc = None,
+                     key_comparator=None, use_disk=False):
+        p_func_java_ref = None
+        comparator_func_java_ref = None
+        if partition_func is not None:
+            p_func_java_ref = self.__env.functions.partition.to_java_ref(
+                partition_func
+            )
+
+        if key_comparator is not None:
+            key_comparator_wrapped = function_wrapper(key_comparator)
+            comparator_func_java_ref = self.__env.functions.comparator.build(
+                key_comparator_wrapped)
+
+        keyed_gather_t_link_java_ref = None
+        if p_func_java_ref is None and comparator_func_java_ref is None:
+            keyed_gather_t_link_java_ref = self.__java_ref.keyedGather()
+        elif comparator_func_java_ref is None and p_func_java_ref is not None:
+            keyed_gather_t_link_java_ref = self.__java_ref.keyedGather(p_func_java_ref)
+        else:
+            keyed_gather_t_link_java_ref = self.__java_ref.keyedGather(
+                p_func_java_ref, comparator_func_java_ref
+            )
+
+        if use_disk:
+            keyed_gather_t_link_java_ref = keyed_gather_t_link_java_ref.useDisk()
+
+        return Tl.TLink(keyed_gather_t_link_java_ref, self.__env)
+
     # TLink functions
+    def map(self, lam):
+        return self.direct().map(lam)
+
+    def flat_map(self, lam):
+        return self.direct().flat_map(lam)
+
+    def sink(self, sink_func):
+        return self.direct().sink(sink_func)
+
+    def compute(self, compute_func):
+        return self.direct().compute(compute_func)
 
     def for_each(self, foreach_func):
         return self.direct().for_each(foreach_func)
