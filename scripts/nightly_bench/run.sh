c9fa5ec928ec3846eaa9e8407361aa4b7682e6d1
Chathura Widanage <chathurawidanage@gmail.com>
2019-12-04 10:07:08 -0500

    Changing version to 0.5.0-SNAPSHOT

--- scripts/nightly_bench/run.sh
+++ scripts/nightly_bench/run.sh
@@ -17,27 +17,27 @@ echo "Twister2 built...."
 cd bazel-bin/scripts/package/ || (echo "Build has failed" && exit)
 
 # extract
-tar -xvzf twister2-0.4.0.tar.gz
+tar -xvzf twister2-0.5.0-SNAPSHOT.tar.gz
 
 # copy hosts
 cd ../../../../
-rm twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/nodes -f
-cp nodes twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/
+rm twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/nodes -f
+cp nodes twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/
 
-rm twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/core.yaml -f
-cp core.yaml twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/
+rm twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/core.yaml -f
+cp core.yaml twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/
 
-rm twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/common/core.yaml -f
-cp common_core.yaml twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/common/core.yaml
+rm twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/common/core.yaml -f
+cp common_core.yaml twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/common/core.yaml
 
-rm twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/mpi.sh -f
-cp mpi.sh twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/
+rm twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/mpi.sh -f
+cp mpi.sh twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/
 
-rm twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/resource.yaml -f
-cp resource.yaml twister2/bazel-bin/scripts/package/twister2-0.4.0/conf/standalone/
+rm twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/resource.yaml -f
+cp resource.yaml twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/conf/standalone/
 
 # navigate to test root
-cd twister2/bazel-bin/scripts/package/twister2-0.4.0/util/test/
+cd twister2/bazel-bin/scripts/package/twister2-0.5.0-SNAPSHOT/util/test/
 
 # run tests
 TIMEOUT=10800
