2f04ad31b4371c7bdec1314b18681b6a17e90455
Chathura Widanage <chathurawidanage@gmail.com>
2019-12-19 13:02:59 -0500

    removing timestamp

--- docs/website/javadocs.sh
+++ docs/website/javadocs.sh
@@ -4,4 +4,4 @@ for f in $(find ../../ -name '*.java'); do javas=$f$space$javas; done
 
 rm static/javadocs -f -r
 mkdir static/javadocs
-javadoc $javas -d static/javadocs -exclude com.twitter.bazel.checkstyle
+javadoc $javas -d static/javadocs -exclude com.twitter.bazel.checkstyle -notimestamp
