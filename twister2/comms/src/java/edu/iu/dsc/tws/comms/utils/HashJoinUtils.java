8e77498740c8e5aefa7daef12461d5d3c5fdb506
Chathura Widanage <chathurawidanage@gmail.com>
2019-12-20 15:07:01 -0500

    Initial disk based hash inner join implementation

--- twister2/comms/src/java/edu/iu/dsc/tws/comms/utils/HashJoinUtils.java
+++ twister2/comms/src/java/edu/iu/dsc/tws/comms/utils/HashJoinUtils.java
@@ -26,11 +26,14 @@ package edu.iu.dsc.tws.comms.utils;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import edu.iu.dsc.tws.api.comms.structs.JoinedTuple;
 import edu.iu.dsc.tws.api.comms.structs.Tuple;
+import edu.iu.dsc.tws.api.exceptions.Twister2RuntimeException;
+import edu.iu.dsc.tws.comms.shuffle.ResettableIterator;
 
 public final class HashJoinUtils {
 
@@ -120,5 +123,106 @@ public final class HashJoinUtils {
       }
     }
     return joinedTuples;
+  }
+
+  public static Iterator<JoinedTuple> innerJoin(ResettableIterator<Tuple<?, ?>> leftIt,
+                                                ResettableIterator<Tuple<?, ?>> rightIt,
+                                                KeyComparatorWrapper comparator) {
+    final double amountToKeepFree = Runtime.getRuntime().maxMemory() * 0.75;
+    ResettableIterator<Tuple<?, ?>> rightItRef = rightIt;
+    return new Iterator<JoinedTuple>() {
+
+      private boolean hashingDone;
+      private Map<Object, List> keyHash = new HashMap<>();
+
+      private JoinedTuple nextJoinTuple;
+
+
+      private void doHashing() {
+        this.keyHash.clear();
+        // building the hash, as long as memory permits
+        while (Runtime.getRuntime().freeMemory() < amountToKeepFree && leftIt.hasNext()) {
+          Tuple<?, ?> nextLeft = leftIt.next();
+          keyHash.computeIfAbsent(nextLeft.getKey(), k -> new ArrayList()).add(nextLeft.getValue());
+        }
+        hashingDone = !leftIt.hasNext();
+
+        if (!hashingDone && this.keyHash.isEmpty()) {
+          // problem!. We have cleared the old hash, yet there's no free memory available to proceed
+          throw new Twister2RuntimeException("Couldn't progress due to memory limitations");
+        }
+      }
+
+      {
+        // initially do hashing & probing
+        doHashing();
+        doProbing();
+      }
+
+      private Tuple<?, ?> nextRightTuple;
+      private List leftListForCurrentKey;
+      private int leftListIndex = 0;
+
+      /**
+       * This method should be guaranteed to create a {@link JoinedTuple}
+       */
+      private void progressProbing() {
+        JoinedTuple joinedTuple = JoinedTuple.of(
+            this.nextRightTuple.getKey(),
+            leftListForCurrentKey.get(leftListIndex),
+            this.nextRightTuple.getValue()
+        );
+        leftListIndex++;
+        if (leftListIndex == leftListForCurrentKey.size()) {
+          nextRightTuple = null;
+          leftListForCurrentKey = null;
+        }
+      }
+
+      private void doProbing() {
+        while (this.nextJoinTuple == null) {
+          if (this.nextRightTuple == null) {
+            if (rightIt.hasNext()) {
+              this.nextRightTuple = rightIt.next();
+              this.leftListForCurrentKey = this.keyHash.get(nextRightTuple.getKey());
+              if (this.leftListForCurrentKey == null) {
+                // not left tuples from left relation to join, skipping this right tuple
+                this.nextRightTuple = null;
+              } else {
+                progressProbing();
+              }
+            } else {
+              // right iterator has reached to an end.
+              if (!hashingDone) {
+                // clear current hash and reset the right iterator
+                doHashing();
+                rightIt.reset();
+              } else {
+                // end of join operation
+                break;
+              }
+            }
+          } else {
+            progressProbing();
+          }
+        }
+      }
+
+      @Override
+      public boolean hasNext() {
+        return this.nextJoinTuple != null;
+      }
+
+      @Override
+      public JoinedTuple next() {
+        if (!hasNext()) {
+          throw new Twister2RuntimeException("Join operation has reached to an end. "
+              + "Use hasNext() to check the status.");
+        }
+        JoinedTuple currentJoinTuple = nextJoinTuple;
+        doProbing();
+        return currentJoinTuple;
+      }
+    };
   }
 }
