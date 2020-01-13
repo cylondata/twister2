7fe6bb765219cf0754f8fb9e4369f0c4a18211bf
Ahmet Uyar <ahmetuyar@gmail.com>
2020-01-13 13:46:17 +0300

    moved job running scripts to conf/kubernetes

--- twister2/config/src/yaml/conf/kubernetes/many-jobs-run.sh
+++ twister2/config/src/yaml/conf/kubernetes/many-jobs-run.sh
@@ -11,5 +11,5 @@ for ((i=0; i<jobs ;i++)); do
   jobName="j${i}"
 
   # start job in background
-  conf/scripts/run-job.sh $jobName $workersPerJob $jobs &
+  conf/kubernetes/run-job.sh $jobName $workersPerJob $jobs &
 done
