c7ba6dacc680a154ed659dd27f006151079a7658
vibhatha <vibhatha@gmail.com>
2020-01-31 11:21:42 -0500

    adding python-support setup and setup script with dev setting

--- twister2/python-support/src/main/python/setup_pip.sh
+++ twister2/python-support/src/main/python/setup_pip.sh
@@ -3,12 +3,35 @@ rm build -R
 rm dist -R
 rm twister2.egg-info -R
 
+# set DEV true for development testing
+# set DEV false for production release
+DEV=true
+# set release version of the API
+VERSION=0.1.27
+
 # Creating distribution
 python3 setup.py bdist_wheel
 
-# Installing Locally
-python3 -m pip install dist/twister2-0.1.16-py3-none-any.whl --user
-#pip install dist/twister2-0.1.14-py3-none-any.whl --user
+if [ ${DEV} = true ]
+then
+  echo "=================Development====================="
+  # Installing Locally
+  #python3 -m pip install dist/twister2-0.1.21-py3-none-any.whl #--user
+  pip3 install dist/twister2-${VERSION}-py3-none-any.whl #--user
+else
+  echo "=================Production================"
+  # Installing Locally
+  python3 -m pip install dist/twister2-${VERSION}-py3-none-any.whl #--user
+  #pip3 install dist/twister2-0.1.27-py3-none-any.whl --user
+  # Uploading to PIP
+  python3 -m twine upload dist/*
+fi
 
-# Uploading to PIP
-python3 -m twine upload dist/*
+
+
+
+
+
+
+
+
