da1d38d3c7afdccc4950e124d576cc26a52f8e47
vibhatha <vibhatha@gmail.com>
2020-02-10 08:35:46 -0500

    adding comm test process spawn

--- deeplearning/pytorch/src/main/python/CommTest.py
+++ deeplearning/pytorch/src/main/python/CommTest.py
@@ -11,27 +11,26 @@
 #  // limitations under the License.
 
 import numpy as np
-
 from twister2.Twister2Environment import Twister2Environment
+from twister2deepnet.deepnet.distributed.ProcessManager import ProcessManager
+
+
 
 env = Twister2Environment(resources=[{"cpu": 1, "ram": 512, "instances": 2}])
 
-comm = env.get_mpi_intercom()
-info = env.get_mpi_info_null()
-int_type = env.get_mpi_datatype_int()
 # Your worker code starts here
-print("Hello from worker {} {}".format(env.worker_id, comm.getRank()))
+print("Hello from worker {}".format(env.worker_id))
 
-int_array = env.gen_java_int_array(size=2)
-spawnargv = env.gen_java_string_array(size=1)
-spawnargv[0] = "/home/vibhatha/github/forks/twister2/deeplearning/pytorch/src/main/resources/bash/twsrunner"
-child = comm.spawn("bash", spawnargv, 2, info, 0, int_array)
+script_path = "/home/vibhatha/github/forks/twister2/deeplearning/pytorch/src/main/resources/bash/pytorchrunner"
+
+pm = ProcessManager(env=env)
+child = pm.spawn(spawnargv=[script_path], maxprocs=2, errorcode=[0,0])
 
 int_data= env.gen_java_int_array(size=4)
 for i in range(4):
     int_data[i] = i * 10
 if env.worker_id == 0:
-    child.send(int_data, 4, int_type, 1, 0)
+    child.send(int_data, 4, env.get_mpi_datatype_int(), 1, 0)
 
 #comm.allReduce(localSum, globalSum, 1, env.get_mpi_datatype_double(), env.get_mpi_op_sum())
 
