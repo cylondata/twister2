2199dad37dc1014a835403e9e0ca9b1a5528d2c9
Chathura Widanage <chathurawidanage@gmail.com>
2020-02-11 15:28:15 -0500

    deployed 0.2.2

--- twister2/tsql/src/java/edu/iu/dsc/tws/tsql/TRel.java
+++
@@ -1,8 +0,0 @@
-package edu.iu.dsc.tws.tsql;
-
-import org.apache.calcite.plan.Convention;
-import org.apache.calcite.rel.RelNode;
-
-public interface TRel extends RelNode {
-  Convention CONVENTION = new Convention.Impl("TWISTER", TRel.class);
-}
--- twister2/tsql/src/java/edu/iu/dsc/tws/tsql/Table.java
+++
@@ -1,4 +0,0 @@
-package edu.iu.dsc.tws.tsql;
-
-public interface Table {
-}
