3da50c119a2837b05134a2f2e232552b71f361df
Chathura Widanage <chathurawidanage@gmail.com>
2020-02-21 13:16:07 -0500

    add more logs

--- twister2/comms/src/java/edu/iu/dsc/tws/comms/ucx/TWSUCXChannel.java
+++ twister2/comms/src/java/edu/iu/dsc/tws/comms/ucx/TWSUCXChannel.java
@@ -110,11 +110,14 @@ public class TWSUCXChannel implements TWSChannel {
     // create end points
     for (JobMasterAPI.WorkerInfo worker : iWorkerController.getJoinedWorkers()) {
       if (worker.getWorkerID() != workerId) {
+        LOG.info("Creating a channel to worker : " + worker.getWorkerID());
         UcpEndpoint ucpEndpoint = ucpWorker.newEndpoint(new UcpEndpointParams().setSocketAddress(
             new InetSocketAddress(worker.getWorkerIP(), worker.getPort())
         ));
         this.endpoints.put(worker.getWorkerID(), ucpEndpoint);
         this.closeables.push(ucpEndpoint);
+      } else {
+        LOG.info("Not creating a channel to id " + worker.getWorkerID());
       }
     }
   }
