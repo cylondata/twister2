0372cf1302dab411a0bcff4d4f4ffb2f4a1119cd
Chathura Widanage <chathurawidanage@gmail.com>
2020-07-17 18:29:34 -0400

    changed to 0.8.0 snapshot

--- twister2/resource-scheduler/src/java/edu/iu/dsc/tws/rsched/job/Twister2Submitter.java
+++ twister2/resource-scheduler/src/java/edu/iu/dsc/tws/rsched/job/Twister2Submitter.java
@@ -170,7 +170,7 @@ public final class Twister2Submitter {
         + "/__   \\__      _(_)___| |_ ___ _ _|___ \\ \n"
         + "  / /\\/\\ \\ /\\ / / / __| __/ _ \\ '__|__) |\n"
         + " / /    \\ V  V /| \\__ \\ ||  __/ |  / __/ \n"
-        + " \\/      \\_/\\_/ |_|___/\\__\\___|_| |_____| v0.7.0\n"
+        + " \\/      \\_/\\_/ |_|___/\\__\\___|_| |_____| v0.8.0-SNAPSHOT\n"
         + "                                         \n"
         + "Job ID      \t  :\t" + job.getJobId() + "\n"
         + "Number of Workers :\t" + job.getNumberOfWorkers() + "\n"
