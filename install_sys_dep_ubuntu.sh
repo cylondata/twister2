de53bc8962d855e7a7f344d589cb405a4122a937
Chathura Widanage <chathurawidanage@gmail.com>
2020-10-02 11:54:16 -0400

    upgrading bazel version in build script

--- install_sys_dep_ubuntu.sh
+++ install_sys_dep_ubuntu.sh
@@ -1,11 +1,11 @@
 install() {
   echo "Installing required tools for bazel..."
-  sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python3
+  sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python3 -y
 
   echo "Installing required tools for twister2..."
   sudo apt-get update
   sudo apt-get install g++ git build-essential automake cmake libtool-bin zip libunwind-setjmp0-dev zlib1g-dev unzip pkg-config python-setuptools -y
-  sudo apt-get install python3-dev python3-pip
+  sudo apt-get install python3-dev python3-pip -y
   sudo pip3 install wheel
 }
 
